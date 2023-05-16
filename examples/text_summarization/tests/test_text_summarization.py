7a0c5f40c41edc9f2f5015e08ae75a5070e92033
Gordon Hart <gordonhart@users.noreply.github.com>
2023-05-16 11:07:23 -0400

    Automated tests for text summarization example integration (#27)
    * Organize examples/text_summarization and add basic import smoke test
    
    * Update CI definition
    
    * Attempt to unbreak checkout with working_directory
    
    * Add automated tests for seed_test_{suite,run}
    
    * Add pre-commit dev dependency
    
    * Standardize script pattern
    
    * Add README stub; use 100-sample subset of full CNN-DailyMail dataset
    
    * Run seed_test_run on only one test suite
    
    * Attempt CPU-only torch install
    
    * Add link to poetry comment explaining torch+cpu install
    
    * Parameterize example tests by resource class; see if 'medium' is sufficient to run text_summarization tests
    
    * Redeclare as matrix
    
    * Update matrix declaration
    
    * Declare 'resource-class' parameter
    
    * Update to use 'CNN-DailyMail' S3 path
    
    * Try 'large' resource class
    
    * Wrap Python version number in quotes

---
+++ examples/text_summarization/tests/test_text_summarization.py
@@ -0,0 +1,26 @@
+import os
+from argparse import Namespace
+from collections.abc import Iterator
+
+import pytest
+from text_summarization.seed_test_run import run as seed_test_run_main
+from text_summarization.seed_test_suite import run as seed_test_suite_main
+
+from kolena._utils.state import kolena_session
+
+
+@pytest.fixture(scope="session", autouse=True)
+def with_init() -> Iterator[None]:
+    with kolena_session(api_token=os.environ["KOLENA_TOKEN"]):
+        yield
+
+
+def test__seed_test_suite() -> None:
+    args = Namespace(dataset_csv="s3://kolena-public-datasets/CNN-DailyMail/metadata/CNN_DailyMail_metadata_100.csv")
+    seed_test_suite_main(args)
+
+
+@pytest.mark.depends(on=["test__seed_test_suite"])
+def test__seed_test_run() -> None:
+    args = Namespace(model_name="ada", test_suite="CNN-DailyMail :: text length", local_csv="none")
+    seed_test_run_main(args)
