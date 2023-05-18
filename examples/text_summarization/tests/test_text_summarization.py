0a92ab7f07f25e804a5504d5d4395c5d5fe6110a
Gordon Hart <gordonhart@users.noreply.github.com>
2023-05-18 17:02:49 -0400

    Only smoke test example integrations in CI (#43)
    * Use tiny 5-record subset of LFW for age_estimation integration tests
    
    * Use 5-sample subset of text summarization dataset
    
    * Use single-sample subset of CNN-DailyMail for text_summarization
    
    * Suffix example integration tests with '__smoke'
    
    * Reference correct name in pytest.mark.depends

--- examples/text_summarization/tests/test_text_summarization.py
+++ examples/text_summarization/tests/test_text_summarization.py
@@ -11,29 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 from argparse import Namespace
-from collections.abc import Iterator
 
 import pytest
-from text_summarization.seed_test_run import run as seed_test_run_main
-from text_summarization.seed_test_suite import run as seed_test_suite_main
-
-from kolena._utils.state import kolena_session
+from text_summarization.seed_test_run import main as seed_test_run_main
+from text_summarization.seed_test_suite import main as seed_test_suite_main
 
 
-@pytest.fixture(scope="session", autouse=True)
-def with_init() -> Iterator[None]:
-    with kolena_session(api_token=os.environ["KOLENA_TOKEN"]):
-        yield
-
-
-def test__seed_test_suite() -> None:
-    args = Namespace(dataset_csv="s3://kolena-public-datasets/CNN-DailyMail/metadata/CNN_DailyMail_metadata_100.csv")
+def test__seed_test_suite__smoke() -> None:
+    args = Namespace(dataset_csv="s3://kolena-public-datasets/CNN-DailyMail/metadata/metadata.tiny1.csv")
     seed_test_suite_main(args)
 
 
-@pytest.mark.depends(on=["test__seed_test_suite"])
-def test__seed_test_run() -> None:
+@pytest.mark.depends(on=["test__seed_test_suite__smoke"])
+def test__seed_test_run__smoke() -> None:
     args = Namespace(model="ada", test_suite="CNN-DailyMail :: text length", local_csv=None)
     seed_test_run_main(args)
