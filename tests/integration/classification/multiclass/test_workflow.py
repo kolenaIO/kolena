27c572de8ed1533eabc484c4779a52670c6a8087
Gordon Hart <gordon.hart2@gmail.com>
2023-06-16 09:13:20 -0400

    Update integration tests; add test for extending Image type

--- tests/integration/classification/multiclass/test_workflow.py
+++ tests/integration/classification/multiclass/test_workflow.py
@@ -11,40 +11,54 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import dataclasses
 import random
+from typing import Optional
 from typing import Type
-from typing import Union
 
+import pydantic
 import pytest
 
 from kolena.classification.multiclass import GroundTruth
 from kolena.classification.multiclass import Inference
-from kolena.classification.multiclass import InferenceLabel
 from kolena.classification.multiclass import Model
 from kolena.classification.multiclass import test
 from kolena.classification.multiclass import TestCase
-from kolena.classification.multiclass import TestSample
 from kolena.classification.multiclass import TestSuite
+from kolena.workflow import Image
+from kolena.workflow import Metadata
 from kolena.workflow.annotation import ClassificationLabel
 from kolena.workflow.annotation import ScoredClassificationLabel
 from tests.integration.helper import fake_locator
 from tests.integration.helper import with_test_prefix
 
 
-@pytest.mark.parametrize("inference_type", [ScoredClassificationLabel, InferenceLabel])
-def test__multiclass_classification__smoke(
-    inference_type: Union[Type[ScoredClassificationLabel], Type[InferenceLabel]],
-) -> None:
-    name = with_test_prefix(f"{__file__} test__multiclass_classification__smoke ({inference_type.__name__})")
-    test_sample = TestSample(locator=fake_locator(0), metadata=dict(example="metadata", values=[1, 2, 3]))
+def test__multiclass_classification__smoke() -> None:
+    name = with_test_prefix(f"{__file__} test__multiclass_classification__smoke")
+    test_sample = Image(locator=fake_locator(0))
     ground_truth = GroundTruth(classification=ClassificationLabel(label="example"))
     test_case = TestCase(f"{name} test case", test_samples=[(test_sample, ground_truth)])
     test_suite = TestSuite(f"{name} test suite", test_cases=[test_case])
 
-    def infer(_: TestSample) -> Inference:
+    def infer(_: Image) -> Inference:
         score = random.random()
         inferences = [("example", 1 - score), ("another", score / 2), ("third", score / 2)]
-        return Inference(inferences=[inference_type(label, conf) for label, conf in inferences])
+        return Inference(inferences=[ScoredClassificationLabel(label=label, score=conf) for label, conf in inferences])
 
     model = Model(f"{name} model", infer=infer)
-    test(model, test_suite)  # TODO: add detailed unit tests for MulticlassClassificationEvaluator
+    test(model, test_suite)  # TODO: add detailed unit tests for evaluate_multiclass_classification
+
+
+@pytest.mark.parametrize("dataclass_type", [dataclasses.dataclass, pydantic.dataclasses.dataclass])
+def test__multiclass_classification__extends_image(dataclass_type: Type) -> None:
+    @dataclass_type(frozen=True)
+    class ExtendsImage(Image):
+        example: str
+        optional: Optional[int] = None
+        metadata: Metadata = dataclasses.field(default_factory=dict)
+
+    test_sample = ExtendsImage(locator="s3://test-bucket/example.png", example="yes", metadata=dict(example=1))
+    ground_truth = GroundTruth(classification=ClassificationLabel(label="example"))
+
+    # any test sample extending kolena.workflow.Image should pass validation
+    TestCase(f"{__file__} test__multiclass_classification__extends_image", test_samples=[(test_sample, ground_truth)])
