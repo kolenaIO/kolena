84e7923a0d0c0b569834c4c4f3657f93e04cd62d
Andrew Shi <andrew@kolena.io>
2023-07-27 14:35:12 -0400

    Share 2D OD example with stratification by bbox area (#159)
    * Share 2D OD example with stratification by bbox area
    
    * Implement area function in seed script for now

--- examples/object_detection_2d/object_detection_2d/seed_test_suite.py
+++ examples/object_detection_2d/object_detection_2d/seed_test_suite.py
@@ -34,6 +34,7 @@ from kolena._experimental.object_detection import GroundTruth
 from kolena._experimental.object_detection import TestCase
 from kolena._experimental.object_detection import TestSample
 from kolena._experimental.object_detection import TestSuite
+from kolena.workflow.annotation import BoundingBox
 from kolena.workflow.annotation import LabeledBoundingBox
 
 
@@ -134,6 +135,50 @@ def seed_test_suite_by_brightness(test_suite_name: str, complete_test_case: Test
     print(f"created test suite '{test_suite.name}' v{test_suite.version}")
 
 
+def seed_test_suite_by_bounding_box_size(test_suite_name: str, complete_test_case: TestCase) -> None:
+    stratification_logic_map = {
+        "small": lambda area: area < 10_000,
+        "medium": lambda area: 10_000 <= area < 60_000,
+        "large": lambda area: 60_000 <= area,
+    }
+
+    def filter_gt_bboxes(gt: GroundTruth, filter_fn: Callable[[float], bool]) -> GroundTruth:
+        bboxes, ignored_bboxes = [], gt.ignored_bboxes
+        for bbox in gt.bboxes:
+            bboxes.append(bbox) if filter_fn(_area(bbox)) else ignored_bboxes.append(bbox)
+        return GroundTruth(bboxes=bboxes, ignored_bboxes=ignored_bboxes)
+
+    # create each test case by stratification
+    test_cases: List[TestCase] = []
+    for name, fn in stratification_logic_map.items():
+        samples_with_filtered_bboxes = []
+        for ts, gt in complete_test_case.iter_test_samples():
+            filtered_ground_truth = filter_gt_bboxes(gt, fn)
+            if filtered_ground_truth.n_bboxes > 0:
+                samples_with_filtered_bboxes.append((ts, filtered_ground_truth))
+        new_test_case = TestCase(
+            f"bounding box size :: {name} :: {DATASET}",
+            test_samples=samples_with_filtered_bboxes,
+            reset=True,
+        )
+        test_cases.append(new_test_case)
+
+    # create the test suite with the complete test case and new test cases
+    test_suite = TestSuite(
+        test_suite_name,
+        description=f"{TEST_SUITE_DESCRIPTION}, stratified by `small`, `medium`, and `large` sized bounding boxes",
+        test_cases=[complete_test_case, *test_cases],
+        reset=True,
+    )
+    print(f"created test suite '{test_suite.name}' v{test_suite.version}")
+
+
+def _area(bbox: BoundingBox) -> float:
+    width = bbox.bottom_right[0] - bbox.top_left[0]
+    height = bbox.bottom_right[1] - bbox.top_left[1]
+    return width * height
+
+
 def main(args: Namespace) -> None:
     kolena.initialize(os.environ["KOLENA_TOKEN"], verbose=True)
 
@@ -141,7 +186,8 @@ def main(args: Namespace) -> None:
 
     # organize test suite names with its generator
     test_suites: List[Tuple[str, Callable[[str, TestCase], TestSuite]]] = [
-        (f"{DATASET} :: transportation brightness [Object Detection]", seed_test_suite_by_brightness),
+        (f"{DATASET} :: transportation by brightness [Object Detection]", seed_test_suite_by_brightness),
+        (f"{DATASET} :: transportation by bounding box size [Object Detection]", seed_test_suite_by_bounding_box_size),
     ]
 
     # create each test suite using the complete test case
