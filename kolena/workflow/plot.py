11e659910a342de386c6a1db0bf5f2b3cc18a3ad
Gordon Hart <gordon.hart2@gmail.com>
2023-08-23 17:18:17 -0400

    Add 'extra' dict to Curve

--- kolena/workflow/plot.py
+++ kolena/workflow/plot.py
@@ -24,6 +24,7 @@ The following plot types are available:
 - [`ConfusionMatrix`][kolena.workflow.ConfusionMatrix]
 """
 from abc import ABCMeta
+from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Sequence
@@ -85,11 +86,18 @@ class Curve(DataObject):
     when e.g. there are multiple curves generated per test case.
     """
 
+    extra: Optional[Dict[str, NumberSeries]] = None
+
     def __post_init_post_parse__(self) -> None:
         if len(self.x) != len(self.y):
             raise ValueError(
                 f"Series 'x' (length: {len(self.x)}) and 'y' (length: {len(self.y)}) have different lengths",
             )
+        for key, series in (self.extra or {}).items():
+            if len(series) != len(self.x):
+                raise ValueError(
+                    f"Series '{key}' (length: {len(series)}) must match length of 'x' and 'y' (length: {len(self.x)})",
+                )
 
 
 @dataclass(frozen=True, config=ValidatorConfig)
