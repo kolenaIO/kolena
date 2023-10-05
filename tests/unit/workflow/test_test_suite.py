418bd3713b5bcfabc4540495aa1cdeffb4c0372e
yingnan zhao <yingnanzhao@yingnans-MacBook-Pro.local>
2023-10-05 18:00:33 -0400

    remove unnecessary imports

--- tests/unit/workflow/test_test_suite.py
+++ tests/unit/workflow/test_test_suite.py
@@ -11,11 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from unittest import mock
-
 import pytest
 
-from kolena.errors import IncorrectUsageError
 from kolena.workflow.test_suite import TestSuite
 
 
