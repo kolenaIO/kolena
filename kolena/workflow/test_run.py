c119c94b0ffa17e0c0c3169d40bef9855cb575f1
yingnan zhao <yingnanzhao@yingnans-MacBook-Pro.local>
2023-10-06 10:30:53 -0400

    use IncorrectUsageError

--- kolena/workflow/test_run.py
+++ kolena/workflow/test_run.py
@@ -43,6 +43,7 @@ from kolena._utils.instrumentation import report_crash
 from kolena._utils.instrumentation import WithTelemetry
 from kolena._utils.serde import from_dict
 from kolena._utils.validators import ValidatorConfig
+from kolena.errors import IncorrectUsageError
 from kolena.errors import InputValidationError
 from kolena.errors import WorkflowMismatchError
 from kolena.workflow import Evaluator
@@ -509,5 +510,7 @@ def test(
     :param reset: Overwrites existing inferences if set.
     """
     if not test_suite.test_cases:
-        raise ValueError(f"test suite '{test_suite.name}' has no test cases, please add test cases to the test suite")
+        raise IncorrectUsageError(
+            f"test suite '{test_suite.name}' has no test cases, please add test cases" f" to the test suite",
+        )
     TestRun(model, test_suite, evaluator, configurations, reset).run()
