ffbe0add60d0e2c7220f24ad0c80106ecd7295e9
Andrew Shi <andrew@kolena.io>
2023-10-09 10:39:52 -0700

    Include activation map upload as part of workflow (#253)
    * Include activation map upload as part of workflow
    
    * Update process to asynchronously batch and upload activation maps

--- examples/semantic_segmentation/semantic_segmentation/seed_test_run.py
+++ examples/semantic_segmentation/semantic_segmentation/seed_test_run.py
@@ -19,6 +19,7 @@ from typing import List
 
 from semantic_segmentation.constants import BUCKET
 from semantic_segmentation.constants import DATASET
+from semantic_segmentation.data_loader import ActivationMapUploader
 from semantic_segmentation.evaluator import evaluate_semantic_segmentation
 from semantic_segmentation.utils import sanitize_model_name
 from semantic_segmentation.workflow import Inference
@@ -28,19 +29,28 @@ from semantic_segmentation.workflow import TestSample
 from semantic_segmentation.workflow import TestSuite
 
 import kolena
+from kolena.workflow.annotation import BitmapMask
 from kolena.workflow.asset import BinaryAsset
 from kolena.workflow.test_run import test
 
 
-def seed_test_run(model_name: str, test_suite_names: List[str]) -> None:
+def seed_test_run(model_name: str, test_suite_names: List[str], out_bucket: str) -> None:
+    sanitized_model_name = sanitize_model_name(model_name)
+    inference_locator_prefix = f"s3://{out_bucket}/{DATASET}/inferences/{sanitized_model_name}"
+    uploader = ActivationMapUploader()  # asynchronously batch and upload activation maps
+
     def infer(test_sample: TestSample) -> Inference:
         basename = test_sample.metadata["basename"]
-        locator = f"s3://{BUCKET}/{DATASET}/results/{sanitized_model_name}/{basename}_person.npy"
-        return Inference(prob=BinaryAsset(locator))
+        prob_array_locator = f"s3://{BUCKET}/{DATASET}/results/{sanitized_model_name}/{basename}_person.npy"
+        activation_map_locator = f"{inference_locator_prefix}/activation/{basename}.png"
+        uploader.submit(prob_array_locator, activation_map_locator)
 
-    sanitized_model_name = sanitize_model_name(model_name)
+        return Inference(
+            prob=BinaryAsset(prob_array_locator),
+            activation_map=BitmapMask(locator=activation_map_locator),
+        )
+
     model = Model(f"{model_name}", infer=infer)
-
     for test_suite_name in test_suite_names:
         test_suite = TestSuite.load(test_suite_name)
         configurations = [SegmentationConfiguration(threshold=0.5)]
@@ -53,12 +63,14 @@ def seed_test_run(model_name: str, test_suite_names: List[str]) -> None:
             reset=True,
         )
 
+    uploader.wait()
+
 
 def main(args: Namespace) -> int:
     kolena.initialize(os.environ["KOLENA_TOKEN"], verbose=True)
     os.environ["KOLENA_MODEL_NAME"] = str(args.model)
     os.environ["KOLENA_OUT_BUCKET"] = str(args.out_bucket)
-    seed_test_run(args.model, args.test_suites)
+    seed_test_run(args.model, args.test_suites, args.out_bucket)
     return 0
 
 
