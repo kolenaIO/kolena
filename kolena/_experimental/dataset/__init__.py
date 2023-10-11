8226cd6fa51192b76102e2f85ac6f5e0c6b60bae
Nitesh Sandal <nitesh@kolena.io>
2023-10-11 10:12:31 -0400

    remove import updates

--- kolena/_experimental/dataset/__init__.py
+++ kolena/_experimental/dataset/__init__.py
@@ -13,59 +13,9 @@
 # limitations under the License.
 from kolena._experimental.dataset._dataset import fetch_dataset
 from kolena._experimental.dataset._dataset import register_dataset
-from kolena.workflow.annotation import BitmapMask
-from kolena.workflow.annotation import BoundingBox
-from kolena.workflow.annotation import Polygon
-from kolena.workflow.annotation import ScoredBoundingBox
-from kolena.workflow.annotation import ScoredBoundingBox3D
-from kolena.workflow.annotation import ScoredClassificationLabel
-from kolena.workflow.annotation import ScoredLabeledBoundingBox
-from kolena.workflow.annotation import ScoredLabeledBoundingBox3D
-from kolena.workflow.annotation import ScoredLabeledPolygon
-from kolena.workflow.annotation import ScoredPolygon
-from kolena.workflow.annotation import SegmentationMask
-from kolena.workflow.asset import Asset
-from kolena.workflow.asset import BinaryAsset
-from kolena.workflow.asset import ImageAsset
-from kolena.workflow.asset import PlainTextAsset
-from kolena.workflow.asset import PointCloudAsset
-from kolena.workflow.ground_truth import GroundTruth
-from kolena.workflow.inference import Inference
-from kolena.workflow.model import Model
-from kolena.workflow.plot import BarPlot
-from kolena.workflow.plot import ConfusionMatrix
-from kolena.workflow.plot import Curve
-from kolena.workflow.plot import CurvePlot
-from kolena.workflow.plot import Histogram
-from kolena.workflow.plot import Plot
 
 
 __all__ = [
     "register_dataset",
     "fetch_dataset",
-    "BoundingBox",
-    "BitmapMask",
-    "SegmentationMask",
-    "Polygon",
-    "ScoredPolygon",
-    "ScoredLabeledBoundingBox",
-    "ScoredLabeledPolygon",
-    "ScoredBoundingBox3D",
-    "ScoredBoundingBox",
-    "ScoredClassificationLabel",
-    "ScoredLabeledBoundingBox3D",
-    "BinaryAsset",
-    "Asset",
-    "ImageAsset",
-    "PointCloudAsset",
-    "PlainTextAsset",
-    "GroundTruth",
-    "Inference",
-    "Model",
-    "Plot",
-    "BarPlot",
-    "CurvePlot",
-    "ConfusionMatrix",
-    "Curve",
-    "Histogram",
 ]
