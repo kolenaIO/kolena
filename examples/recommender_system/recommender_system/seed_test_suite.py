de0bcf112ec361568ac8f7eda33ebe2fcc890ffe
Andy <andy@kolena.io>
2023-10-19 09:57:27 -0400

    initial demo seeding

--- examples/recommender_system/recommender_system/seed_test_suite.py
+++ examples/recommender_system/recommender_system/seed_test_suite.py
@@ -11,3 +11,131 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+import sys
+import time
+from argparse import ArgumentParser
+from argparse import Namespace
+from typing import List
+from typing import Tuple
+
+import pandas as pd
+from recommender_system.workflow import GroundTruth
+from recommender_system.workflow import TestCase
+from recommender_system.workflow import TestSample
+from recommender_system.workflow import TestSuite
+
+import kolena
+
+BUCKET = "kolena-public-datasets"
+DATASET = "movielens"
+
+
+def create_test_case_for_genre(
+    test_samples_and_ground_truths: List[Tuple[TestCase, GroundTruth]],
+    genre: str,
+) -> TestCase:
+    name = f"genre :: {genre} :: {DATASET}"
+    description = f"demographic subset of {DATASET} with source data labeled as genre={genre}"
+
+    # filter down to only test samples matching this genre
+    test_samples = [(ts, gt) for ts, gt in test_samples_and_ground_truths if genre in ts.metadata["genres"]]
+
+    test_case = TestCase(
+        name=name,
+        description=description,
+        test_samples=test_samples,
+        reset=True,
+    )
+
+    return test_case
+
+
+def main(args: Namespace) -> int:
+    kolena.initialize(os.environ["KOLENA_TOKEN"], verbose=True)
+
+    t0 = time.time()
+
+    df_ratings = pd.read_csv(args.ratings_csv)
+    df_movies = pd.read_csv(args.movies_csv)
+
+    def process_metadata(record, f):
+        value = getattr(record, f)
+        return value if f != "genres" else value.split("|")
+
+    metadata_by_movie_id = {}
+    for record in df_movies.itertuples(index=False):
+        fields = set(record._fields)
+        fields.remove("movieId")
+
+        metadata_by_movie_id[record.movieId] = {f: process_metadata(record, f) for f in fields}
+
+    test_samples_and_ground_truths = [
+        (
+            TestSample(user_id=record.userId, movie_id=record.movieId, metadata=metadata_by_movie_id[record.movieId]),
+            GroundTruth(rating=record.rating),
+        )
+        for record in df_ratings.itertuples(index=False)
+    ]
+
+    t1 = time.time()
+    complete_test_case = TestCase(
+        name=f"ml-100k complete :: {DATASET}",
+        description=f"All images in {DATASET} dataset",
+        test_samples=test_samples_and_ground_truths,
+        reset=True,
+    )
+    print(f"created baseline test case '{complete_test_case.name}' in {time.time() - t1:0.3f} seconds")
+
+    # Metadata Test Cases
+    genre_subsets = [
+        "Adventure",
+        "Animation",
+        "Children",
+        "Comedy",
+        "Fantasy",
+        "Romance",
+        "Drama",
+        "Action",
+        "Crime",
+        "Thriller",
+        "Horror",
+        "Mystery",
+        "Sci-Fi",
+        "IMAX",
+        "Documentary",
+        "War",
+        "Musical",
+        "Western",
+        "Film-Noir",
+        "(no genres listed)",
+    ]
+
+    t2 = time.time()
+    test_cases: List[TestCase] = []
+    for genre in genre_subsets:
+        test_case = create_test_case_for_genre(test_samples_and_ground_truths, genre)
+        test_cases.append(test_case)
+        print(f"created test case '{test_case.name}' in {time.time() - t2:0.3f} seconds")
+
+    test_suite = TestSuite(
+        name=f"fr 1:1 :: {DATASET}",
+        test_cases=[complete_test_case, *test_cases],
+        reset=True,
+    )
+    print(f"created test suite '{test_suite}' in {time.time() - t0:0.3f} seconds")
+
+
+if __name__ == "__main__":
+    ap = ArgumentParser()
+    ap.add_argument(
+        "--ratings_csv",
+        type=str,
+        default=f"s3://{BUCKET}/{DATASET}/meta/ratings.100k.csv",
+    )
+    ap.add_argument(
+        "--movies_csv",
+        type=str,
+        default=f"s3://{BUCKET}/{DATASET}/meta/movies.csv",
+    )
+    sys.exit(main(ap.parse_args()))
--- examples/recommender_system/recommender_system/workflow.py
+++ examples/recommender_system/recommender_system/workflow.py
@@ -1,3 +1,4 @@
+# build out the workflow for the FR problem which is already supported in kolena.fr as a pre-built workflow.
 # Copyright 2021-2023 Kolena Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,3 +12,77 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import dataclasses
+from typing import Optional
+
+from pydantic.dataclasses import dataclass
+
+from kolena.workflow import define_workflow
+from kolena.workflow import EvaluatorConfiguration
+from kolena.workflow import GroundTruth as BaseGroundTruth
+from kolena.workflow import TestSample as BaseTestSample
+from kolena.workflow import Inference as BaseInference
+from kolena.workflow import Metadata
+from kolena.workflow import MetricsTestCase
+from kolena.workflow import MetricsTestSample
+from kolena.workflow import MetricsTestSuite
+
+
+@dataclass(frozen=True)
+class TestSample(BaseTestSample):
+    """Test sample type for Recommender System Workflow"""
+
+    user_id: int
+    movie_id: int
+    metadata: Metadata = dataclasses.field(default_factory=dict)
+
+
+@dataclass(frozen=True)
+class GroundTruth(BaseGroundTruth):
+    rating: float
+
+
+@dataclass(frozen=True)
+class Inference(BaseInference):
+    rating: float
+
+
+workflow, TestCase, TestSuite, Model = define_workflow(
+    "Recommender System",
+    TestSample,
+    GroundTruth,
+    Inference,
+)
+
+
+@dataclass(frozen=True)
+class TestSampleMetrics(MetricsTestSample):
+    is_correct: bool
+    rating: float
+    threshold: Optional[float] = None
+
+
+@dataclass(frozen=True)
+class TestCaseMetrics(MetricsTestCase):
+    RMSE: float  # Root Mean Squared Error
+    MAE: float  # Mean Absolute Error
+    Precision: float  # Mean Average Precision
+    Recall: float  # Mean Average Recall
+
+    # Coverage
+
+
+@dataclass(frozen=True)
+class TestSuiteMetrics(MetricsTestSuite):
+    macro_RMSE: float
+    macro_MAE: float
+    mAP: float  # Mean Average Precision
+    mAR: float  # Mean Average Recall
+
+
+@dataclass(frozen=True)
+class ThresholdConfiguration(EvaluatorConfiguration):
+    rating_threshold: float
+
+    def display_name(self) -> str:
+        return f"Rating Threshold: {self.rating_threshold}"
