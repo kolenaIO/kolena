c91931d4658f7154db01024f436559c6f198b78a
Nan Wei <nan@kolena.io>
2023-10-24 13:56:03 -0400

    use pydantic dataclass

--- kolena/_api/v1/event_tracking.py
+++ kolena/_api/v1/event_tracking.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from dataclasses import dataclass
 from enum import Enum
 from typing import Dict
 from typing import Optional
+
+from pydantic.dataclasses import dataclass
 
 
 class Tracking:
