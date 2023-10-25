12a05949a175b11ccd08b844aa2816ccdc1c8867
nankolena <145366880+nankolena@users.noreply.github.com>
2023-10-25 15:48:42 -0400

    [KOL-2675] add event api to kolena client (#271)
    * add event tracking to client initialization
    
    * rename token generation event name
    
    * create event tracking decorator
    
    * add event tracking to test cases
    
    * add event tracking to test suites
    
    * add event tracking to model and test run
    
    * fix style error
    
    * fix pre-commit style error
    
    * rename event name for load all test suites
    
    * remove started/failed/succeeded event tracking
    
    * track api call duration
    
    * use pydantic dataclass
    
    * add float to mixpanel metadata
    
    * add all primitive dtypes to mixpanel metadata
    
    * rename event api functions
    
    * rename tracking to recording
    
    * update path for event api
    
    * ignore exceptions in event api

---
+++ kolena/_api/v1/event.py
@@ -0,0 +1,59 @@
+# Copyright 2021-2023 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from enum import Enum
+from typing import Dict
+from typing import Optional
+from typing import Union
+
+from pydantic.dataclasses import dataclass
+from pydantic.types import StrictBool
+from pydantic.types import StrictFloat
+from pydantic.types import StrictInt
+from pydantic.types import StrictStr
+
+
+class EventAPI:
+    class Path(str, Enum):
+        PROFILE = "/event/profile"
+        EVENT = "/event"
+
+    class Event(str, Enum):
+        # auth
+        GENERATE_TOKEN = "generate-token"
+
+        # test case
+        CREATE_TEST_CASE = "create-test-case"
+        LOAD_TEST_CASE = "load-test-case"
+        LOAD_TEST_CASE_SAMPLES = "load-test-case-samples"
+        EDIT_TEST_CASE = "edit-test-case"
+        INIT_MANY_TEST_CASES = "initialize-many-test-cases"
+
+        # test suite
+        CREATE_TEST_SUITE = "create-test-suite"
+        LOAD_TEST_SUITE = "load-test-suite"
+        LOAD_ALL_TEST_SUITES = "load-all-test-suites"
+        EDIT_TEST_SUITE = "edit-test-suite"
+        LOAD_TEST_SUITE_SAMPLES = "load-test-suite-samples"
+
+        # model
+        CREATE_MODEL = "create-model"
+        LOAD_MODEL = "load-model"
+
+        # test run
+        EXECUTE_TEST_RUN = "execute-test-run"
+
+    @dataclass(frozen=True)
+    class RecordEventRequest:
+        event_name: str
+        additional_metadata: Optional[Dict[str, Union[StrictInt, StrictFloat, StrictStr, StrictBool, None]]] = None
