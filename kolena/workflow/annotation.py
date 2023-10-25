76a225cc23b0fd5602505fd35a8fe026e874a960
Gordon Hart <gordon.hart2@gmail.com>
2023-10-25 09:21:13 -0400

    Clean up some internal usage of ClassificationLabel

--- kolena/workflow/annotation.py
+++ kolena/workflow/annotation.py
@@ -23,7 +23,7 @@ The following annotation types are available:
 - [`BoundingBox3D`][kolena.workflow.annotation.BoundingBox3D]
 - [`SegmentationMask`][kolena.workflow.annotation.SegmentationMask]
 - [`BitmapMask`][kolena.workflow.annotation.BitmapMask]
-- [`ClassificationLabel`][kolena.workflow.annotation.ClassificationLabel]
+- [`Label`][kolena.workflow.annotation.Label]
 
 For example, when viewing images in the Studio, any annotations (such as lists of
 [`BoundingBox`][kolena.workflow.annotation.BoundingBox] objects) present in the
@@ -309,7 +309,7 @@ class Label(Annotation):
 
     @staticmethod
     def _data_type() -> _AnnotationType:
-        return _AnnotationType.CLASSIFICATION_LABEL
+        return _AnnotationType.LABEL
 
 
 ClassificationLabel = Label
@@ -345,6 +345,8 @@ _ANNOTATION_TYPES = [
     ScoredLabeledBoundingBox3D,
     SegmentationMask,
     BitmapMask,
+    Label,
+    ScoredLabel,
     ClassificationLabel,
     ScoredClassificationLabel,
 ]
