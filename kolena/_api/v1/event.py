1afc1c668cf3fa908b77ee2d828e179e19bd23ad
nankolena <145366880+nankolena@users.noreply.github.com>
2023-10-27 17:12:05 -0400

    rename client event names for consistency (#293)
    * rename client initialization event name
    
    * rename all events as <noun>-<verb> for consistency
    
    * add sdk- prefix to all event names

--- kolena/_api/v1/event.py
+++ kolena/_api/v1/event.py
@@ -30,28 +30,28 @@ class EventAPI:
 
     class Event(str, Enum):
         # auth
-        GENERATE_TOKEN = "generate-token"
+        INITIALIZE_SDK_CLIENT = "sdk-client-initialized"
 
         # test case
-        CREATE_TEST_CASE = "create-test-case"
-        LOAD_TEST_CASE = "load-test-case"
-        LOAD_TEST_CASE_SAMPLES = "load-test-case-samples"
-        EDIT_TEST_CASE = "edit-test-case"
-        INIT_MANY_TEST_CASES = "initialize-many-test-cases"
+        CREATE_TEST_CASE = "sdk-test-case-created"
+        LOAD_TEST_CASE = "sdk-test-case-loaded"
+        LOAD_TEST_CASE_SAMPLES = "sdk-test-case-samples-loaded"
+        EDIT_TEST_CASE = "sdk-test-case-edited"
+        INIT_MANY_TEST_CASES = "sdk-many-test-cases-initialized"
 
         # test suite
-        CREATE_TEST_SUITE = "create-test-suite"
-        LOAD_TEST_SUITE = "load-test-suite"
-        LOAD_ALL_TEST_SUITES = "load-all-test-suites"
-        EDIT_TEST_SUITE = "edit-test-suite"
-        LOAD_TEST_SUITE_SAMPLES = "load-test-suite-samples"
+        CREATE_TEST_SUITE = "sdk-test-suite-created"
+        LOAD_TEST_SUITE = "sdk-test-suite-loaded"
+        LOAD_ALL_TEST_SUITES = "sdk-all-test-suites-loaded"
+        EDIT_TEST_SUITE = "sdk-test-suite-edited"
+        LOAD_TEST_SUITE_SAMPLES = "sdk-test-suite-samples-loaded"
 
         # model
-        CREATE_MODEL = "create-model"
-        LOAD_MODEL = "load-model"
+        CREATE_MODEL = "sdk-model-created"
+        LOAD_MODEL = "sdk-model-loaded"
 
         # test run
-        EXECUTE_TEST_RUN = "execute-test-run"
+        EXECUTE_TEST_RUN = "sdk-test-run-executed"
 
     @dataclass(frozen=True)
     class RecordEventRequest:
