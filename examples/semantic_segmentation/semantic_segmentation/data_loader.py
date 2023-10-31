da71a93777d10f889a6f2808be905006c9bca789
Connor Soulliere <connor@kolena.io>
2023-10-31 14:56:15 -0400

    Improve semantic segmentation example (#291)
    * Initial performance improvements
    
    - Use 32 workers in DataLoader class since it is IO bound.
    - Use default workers in ActivationMapUploader since creating the
    activation map is CPU intensive.
    - Remove uneccesary splicing of ground truth and inference to locator.
    
    * Reuse activation maps from public s3 for seed_test_run
    
    Speeds up `seed_test_run` by using pregenerated activation maps.
    Moves the activation map generation and uploading to a seperate
    folder.
    
    * Improve result mask upload and test computation
    
    Improves result mask upload by removing needless batching and
    splicing.
    Improves metric calculation by reusing result of
    `compute_sklearn_arrays`.
    
    * Update README with seed_activation_map script
    
    * Update README with memory usage warning

--- examples/semantic_segmentation/semantic_segmentation/data_loader.py
+++ examples/semantic_segmentation/semantic_segmentation/data_loader.py
@@ -11,24 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import functools
 from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import wait
+from typing import Iterator
 from typing import List
 from typing import Tuple
 
 import numpy as np
 from pydantic.dataclasses import dataclass
-from semantic_segmentation.utils import create_bitmap
 from semantic_segmentation.utils import download_binary_array
 from semantic_segmentation.utils import download_mask
 from semantic_segmentation.utils import upload_image
-from semantic_segmentation.utils import upload_image_buffer
 from semantic_segmentation.workflow import GroundTruth
 from semantic_segmentation.workflow import Inference
 from semantic_segmentation.workflow import Label
 from semantic_segmentation.workflow import TestSample
 
+from kolena._utils.log import progress_bar
 from kolena.workflow.annotation import SegmentationMask
 
 
@@ -40,40 +38,33 @@ class ResultMask:
 
 
 ResultMasks = Tuple[ResultMask, ResultMask, ResultMask]
-ResultMasksByLocator = Tuple[str, ResultMask, ResultMask, ResultMask]
 
 
 class DataLoader:
     def __init__(self):
-        self.pool = ThreadPoolExecutor()
+        self.pool = ThreadPoolExecutor(max_workers=32)
 
-    def load_batch(
+    def download_masks(
         self,
-        batch: List[Tuple[TestSample, GroundTruth, Inference]],
-    ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
-        def load(ts: TestSample, gt: GroundTruth, inf: Inference) -> Tuple[str, np.ndarray, np.ndarray]:
+        ground_truths: List[GroundTruth],
+        inferences: List[Inference],
+    ) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
+        def load(gt: GroundTruth, inf: Inference) -> Tuple[np.ndarray, np.ndarray]:
             inf_prob = download_binary_array(inf.prob.locator)
             gt_mask = download_mask(gt.mask.locator)
             gt_mask[gt_mask != 1] = 0  # binarize gt_mask
-            return ts.locator, gt_mask, inf_prob
+            return gt_mask, inf_prob
 
-        futures = [self.pool.submit(functools.partial(load, *item)) for item in batch]
-        successes, failures = wait(futures)
-        if len(failures) != 0:
-            exceptions = ", ".join([str(failure.exception()) for failure in failures])
-            raise RuntimeError(f"failed to load {len(failures)} samples: {exceptions}")
+        return progress_bar(self.pool.map(load, ground_truths, inferences), total=len(ground_truths))
 
-        # splice together correct ordering
-        gt_by_locator = {result[0]: result[1] for result in [f.result() for f in successes]}
-        inf_by_locator = {result[0]: result[2] for result in [f.result() for f in successes]}
-        return [gt_by_locator[ts.locator] for ts, _, _ in batch], [inf_by_locator[ts.locator] for ts, _, _ in batch]
-
-    def upload_batch(
+    def upload_masks(
         self,
         locator_prefix: str,
-        batch: List[Tuple[TestSample, np.ndarray, np.ndarray]],
+        test_samples: List[TestSample],
+        gt_masks: List[np.ndarray],
+        inf_masks: List[np.ndarray],
     ) -> List[ResultMasks]:
-        def upload(ts: TestSample, gt_mask: np.ndarray, inf_mask: np.ndarray) -> ResultMasksByLocator:
+        def upload(ts: TestSample, gt_mask: np.ndarray, inf_mask: np.ndarray) -> ResultMasks:
             def upload_result_mask(category: str, mask: np.ndarray) -> ResultMask:
                 locator = f"{locator_prefix}/{category}/{ts.metadata['basename']}.png"
                 upload_image(locator, mask)
@@ -86,49 +77,11 @@ class DataLoader:
             tp = upload_result_mask("TP", np.where(gt_mask != inf_mask, 0, inf_mask))
             fp = upload_result_mask("FP", np.where(gt_mask == inf_mask, 0, inf_mask))
             fn = upload_result_mask("FN", np.where(gt_mask == inf_mask, 0, gt_mask))
-            return ts.locator, tp, fp, fn
+            return tp, fp, fn
 
-        futures = [self.pool.submit(functools.partial(upload, *item)) for item in batch]
-        successes, failures = wait(futures)
-        if len(failures) != 0:
-            exceptions = ", ".join([str(failure.exception()) for failure in failures])
-            raise RuntimeError(f"failed to upload {len(failures)} samples: {exceptions}")
-
-        # splice together correct ordering
-        result_masks_by_locator = {result[0]: tuple(result[1:]) for result in [f.result() for f in successes]}
-        return [result_masks_by_locator[ts.locator] for ts, _, _ in batch]
-
-
-class ActivationMapUploader:
-    def __init__(self):
-        self.pool = ThreadPoolExecutor(max_workers=32)
-        self.futures = {}
-
-    def wait(self) -> None:
-        if len(self.futures) == 0:
-            return
-
-        print(f"waiting for uploads for {len(self.futures)} activation maps")
-        successes, failures = wait(list(self.futures.values()))
-
-        if len(failures) != 0:
-            exceptions = ", ".join([str(failure.exception()) for failure in failures])
-            raise RuntimeError(f"failed to load {len(failures)} samples: {exceptions}")
-        elif len(successes) != len(self.futures):
-            raise RuntimeError(f"missing uploads for {len(self.futures) - len(successes)} activation maps")
-        else:
-            print(f"completed upload of {len(self.futures)} activation maps")
-
-    def submit(self, prob_array_locator: str, activation_map_locator: str) -> None:
-        if activation_map_locator in self.futures.keys():
-            # already being processed, skip
-            return
-        future = self.pool.submit(
-            functools.partial(self.process_activation_map, prob_array_locator, activation_map_locator),
+        return list(
+            progress_bar(
+                self.pool.map(upload, test_samples, gt_masks, inf_masks),
+                total=len(test_samples),
+            ),
         )
-        self.futures[activation_map_locator] = future
-
-    def process_activation_map(self, prob_array_locator: str, activation_map_locator: str) -> None:
-        prob_array = download_binary_array(prob_array_locator)
-        activation_map = create_bitmap(prob_array)
-        upload_image_buffer(activation_map_locator, activation_map)
--- examples/semantic_segmentation/semantic_segmentation/seed_activation_map.py
+++ examples/semantic_segmentation/semantic_segmentation/seed_activation_map.py
@@ -11,60 +11,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 import sys
 from argparse import ArgumentParser
 from argparse import Namespace
 from typing import List
 
+from semantic_segmentation.activation_map_uploader import ActivationMapUploader
 from semantic_segmentation.constants import BUCKET
 from semantic_segmentation.constants import DATASET
-from semantic_segmentation.evaluator import evaluate_semantic_segmentation
 from semantic_segmentation.utils import sanitize_model_name
-from semantic_segmentation.workflow import Inference
-from semantic_segmentation.workflow import Model
-from semantic_segmentation.workflow import SegmentationConfiguration
-from semantic_segmentation.workflow import TestSample
 from semantic_segmentation.workflow import TestSuite
 
 import kolena
-from kolena.workflow.annotation import BitmapMask
-from kolena.workflow.asset import BinaryAsset
-from kolena.workflow.test_run import test
 
 
-def seed_test_run(model_name: str, test_suite_names: List[str], out_bucket: str) -> None:
+def seed_activation_map(model_name: str, test_suite_names: List[str], out_bucket: str) -> None:
     sanitized_model_name = sanitize_model_name(model_name)
-    s3_prefix = f"s3://{BUCKET}/{DATASET}"
+    inf_locator_prefix = f"s3://{BUCKET}/{DATASET}/results/{sanitized_model_name}/"
+    map_locator_prefix = f"s3://{out_bucket}/{DATASET}/inferences/{sanitized_model_name}/activation/"
+    uploader = ActivationMapUploader(inf_locator_prefix, map_locator_prefix)
+    test_sample_names = set()
 
-    def infer(test_sample: TestSample) -> Inference:
-        basename = test_sample.metadata["basename"]
-        prob_array_locator = f"{s3_prefix}/results/{sanitized_model_name}/{basename}_person.npy"
-        activation_map_locator = f"{s3_prefix}/inferences/{sanitized_model_name}/activation/{basename}.png"
-        return Inference(
-            prob=BinaryAsset(prob_array_locator),
-            activation_map=BitmapMask(locator=activation_map_locator),
-        )
-
-    model = Model(f"{model_name}", infer=infer)
     for test_suite_name in test_suite_names:
         test_suite = TestSuite.load(test_suite_name)
-        configurations = [SegmentationConfiguration(threshold=0.5)]
+        for _, test_samples in test_suite.load_test_samples():
+            test_sample_names.update([ts.metadata["basename"] for ts in test_samples])
 
-        test(
-            model,
-            test_suite,
-            evaluate_semantic_segmentation,
-            configurations=configurations,
-            reset=True,
-        )
+    uploader.submit(test_sample_names)
 
 
 def main(args: Namespace) -> int:
     kolena.initialize(verbose=True)
-    os.environ["KOLENA_MODEL_NAME"] = str(args.model)
-    os.environ["KOLENA_OUT_BUCKET"] = str(args.out_bucket)
-    seed_test_run(args.model, args.test_suites, args.out_bucket)
+    seed_activation_map(args.model, args.test_suites, args.out_bucket)
     return 0
 
 
@@ -84,7 +62,7 @@ if __name__ == "__main__":
     ap.add_argument(
         "--out_bucket",
         required=True,
-        help="Name of AWS S3 bucket with write access to upload result masks to.",
+        help="Name of AWS S3 bucket with write access to upload activation maps to.",
     )
 
     sys.exit(main(ap.parse_args()))
