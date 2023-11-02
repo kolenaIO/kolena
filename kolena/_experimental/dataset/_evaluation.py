57d3008d6725589330240e8e088a514c41cd735e
Yifan Wu <yifan@kolena.io>
2023-11-02 11:24:31 -0400

    chore: trim

--- kolena/_experimental/dataset/_evaluation.py
+++ kolena/_experimental/dataset/_evaluation.py
@@ -14,7 +14,6 @@
 import json
 from dataclasses import asdict
 from typing import Any
-from typing import Callable
 from typing import Dict
 from typing import Iterator
 from typing import List
@@ -24,21 +23,14 @@ from typing import Union
 
 import pandas as pd
 
-from kolena._api.v2.evaluation import LoadMetricsRequest
-from kolena._api.v2.evaluation import Path as EvaluationPath
-from kolena._api.v2.evaluation import UploadMetricsRequest
-from kolena._api.v2.model import LoadInferencesRequest
 from kolena._api.v2.model import LoadResultsRequest
 from kolena._api.v2.model import Path
-from kolena._api.v2.model import UploadInferencesRequest
 from kolena._api.v2.model import UploadResultsRequest
 from kolena._experimental.dataset._dataset import _iter_dataset_raw
 from kolena._experimental.dataset._dataset import _to_deserialized_dataframe
 from kolena._experimental.dataset._dataset import _to_serialized_dataframe
 from kolena._experimental.dataset.common import COL_DATAPOINT
 from kolena._experimental.dataset.common import COL_EVAL_CONFIG
-from kolena._experimental.dataset.common import COL_INFERENCE
-from kolena._experimental.dataset.common import COL_METRICS
 from kolena._experimental.dataset.common import COL_RESULT
 from kolena._experimental.dataset.common import validate_batch_size
 from kolena._utils import krequests_v2 as krequests
@@ -52,39 +44,14 @@ from kolena.errors import IncorrectUsageError
 
 
 TYPE_EVALUATION_CONFIG = Optional[Dict[str, Any]]
-INFER_FUNC_TYPE = Callable[[pd.DataFrame], pd.DataFrame]
-EVAL_FUNC_TYPE = Callable[[pd.DataFrame, pd.DataFrame, Optional[TYPE_EVALUATION_CONFIG]], pd.DataFrame]
-TEST_INFER_TYPE = Optional[Union[INFER_FUNC_TYPE, pd.DataFrame]]
-TEST_EVAL_TYPE = Optional[Union[EVAL_FUNC_TYPE, pd.DataFrame]]
-TEST_EVAL_CONFIGS_TYPE = Optional[Union[TYPE_EVALUATION_CONFIG, List[TYPE_EVALUATION_CONFIG]]]
 TEST_ON_TYPE = Optional[Union[str, List[str]]]
 
 
 def _fetch_dataset(dataset: str) -> pd.DataFrame:
     df_data_batch = list(_iter_dataset_raw(dataset))
-    return pd.concat(df_data_batch) if df_data_batch else pd.DataFrame(columns=["id", COL_DATAPOINT])
-
-
-def _iter_inference_raw(dataset: str, model: str, batch_size: int) -> Iterator[pd.DataFrame]:
-    validate_batch_size(batch_size)
-    init_request = LoadInferencesRequest(dataset=dataset, model=model, batch_size=batch_size)
-    yield from _BatchedLoader.iter_data(
-        init_request=init_request,
-        endpoint_path=Path.LOAD_INFERENCES.value,
-        df_class=None,
-        endpoint_api_version=API_V2,
-    )
-
-
-def _iter_metrics_raw(dataset: str, model: str, batch_size: int) -> Iterator[pd.DataFrame]:
-    validate_batch_size(batch_size)
-    init_request = LoadMetricsRequest(dataset=dataset, model=model, batch_size=batch_size)
-    yield from _BatchedLoader.iter_data(
-        init_request=init_request,
-        endpoint_path=EvaluationPath.LOAD_METRICS.value,
-        df_class=None,
-        endpoint_api_version=API_V2,
-    )
+    df_datapoints = pd.concat(df_data_batch) if df_data_batch else pd.DataFrame(columns=["id", COL_DATAPOINT])
+    df_datapoints.rename(columns={"id": "datapoint_id"}, inplace=True)
+    return df_datapoints
 
 
 def _iter_result_raw(dataset: str, model: str, batch_size: int) -> Iterator[pd.DataFrame]:
@@ -98,28 +65,6 @@ def _iter_result_raw(dataset: str, model: str, batch_size: int) -> Iterator[pd.D
     )
 
 
-def _fetch_inferences(dataset: str, model: str) -> pd.DataFrame:
-    df_inference_batch = list(_iter_inference_raw(dataset, model, batch_size=BatchSize.LOAD_RECORDS))
-    return (
-        pd.concat(df_inference_batch)
-        if df_inference_batch
-        else pd.DataFrame(
-            columns=["datapoint_id", COL_DATAPOINT, "inference_id", COL_INFERENCE],
-        )
-    )
-
-
-def fetch_inferences(dataset: str, model: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """
-    Fetch inferences given dataset name and model name.
-    """
-    df_data = _fetch_inferences(dataset, model)
-    df_datapoints = _to_deserialized_dataframe(df_data, column=COL_DATAPOINT)
-    df_inferences = _to_deserialized_dataframe(df_data, column=COL_INFERENCE)
-
-    return df_datapoints, df_inferences
-
-
 def _fetch_results(dataset: str, model: str) -> pd.DataFrame:
     df_result_batch = list(_iter_result_raw(dataset, model, batch_size=BatchSize.LOAD_RECORDS))
     return (
@@ -129,51 +74,6 @@ def _fetch_results(dataset: str, model: str) -> pd.DataFrame:
             columns=["datapoint_id", COL_DATAPOINT, COL_RESULT, COL_EVAL_CONFIG],
         )
     )
-
-
-def _upload_inferences(model: str, df: pd.DataFrame) -> None:
-    load_uuid = init_upload().uuid
-    upload_data_frame(
-        df=df[["id", COL_INFERENCE]],
-        batch_size=BatchSize.UPLOAD_RECORDS.value,
-        load_uuid=load_uuid,
-    )
-    request = UploadInferencesRequest(model=model, uuid=load_uuid)
-    response = krequests.post(Path.UPLOAD_INFERENCES, json=asdict(request))
-    krequests.raise_for_status(response)
-
-
-def _process_metrics(
-    df: pd.DataFrame,
-    all_metrics: List[Tuple[Optional[TYPE_EVALUATION_CONFIG], pd.DataFrame]],
-) -> pd.DataFrame:
-    df_metrics_by_eval = []
-    for eval_config, df_metrics in all_metrics:
-        df_metrics_eval = _to_serialized_dataframe(df_metrics, column=COL_METRICS)
-        df_metrics_eval[COL_EVAL_CONFIG] = json.dumps(eval_config) if eval_config is not None else None
-        df_metrics_by_eval.append(pd.concat([df["inference_id"], df_metrics_eval], axis=1))
-    df_metrics = (
-        pd.concat(df_metrics_by_eval, ignore_index=True)
-        if df_metrics_by_eval
-        else pd.DataFrame(
-            columns=["inference_id", COL_METRICS],
-        )
-    )
-    return df_metrics
-
-
-def _upload_metrics(
-    df: pd.DataFrame,
-) -> int:
-    if df["inference_id"].isnull().any():
-        raise IncorrectUsageError("cannot upload metrics without inference")
-
-    load_uuid = init_upload().uuid
-    upload_data_frame(df=df, batch_size=BatchSize.UPLOAD_RECORDS.value, load_uuid=load_uuid)
-    request = UploadMetricsRequest(uuid=load_uuid)
-    response = krequests.post(EvaluationPath.UPLOAD_METRICS, json=asdict(request))
-    krequests.raise_for_status(response)
-    return len(df)
 
 
 def _process_results(
@@ -214,14 +114,7 @@ def fetch_results(
     """
     Fetch results given dataset name and model name.
     """
-    df_results_batch = list(_iter_result_raw(dataset, model, batch_size=BatchSize.LOAD_RECORDS))
-    df = (
-        pd.concat(df_results_batch)
-        if df_results_batch
-        else pd.DataFrame(
-            columns=[COL_DATAPOINT, COL_RESULT, COL_EVAL_CONFIG],
-        )
-    )
+    df = _fetch_results(dataset, model)
 
     df_datapoints = _to_deserialized_dataframe(df, column=COL_DATAPOINT)
     eval_configs = df[COL_EVAL_CONFIG].unique()
@@ -238,39 +131,7 @@ def fetch_results(
     return df_datapoints, df_results_by_eval
 
 
-def fetch_evaluation_results(
-    dataset: str,
-    model: str,
-) -> List[Tuple[Dict[str, Any], pd.DataFrame, pd.DataFrame, pd.DataFrame]]:
-    """
-    Fetch evaluation results given dataset name and model name.
-    """
-    df_results_batch = list(_iter_metrics_raw(dataset, model, batch_size=BatchSize.LOAD_RECORDS))
-    df = (
-        pd.concat(df_results_batch)
-        if df_results_batch
-        else pd.DataFrame(
-            columns=[COL_DATAPOINT, COL_INFERENCE, COL_METRICS, COL_EVAL_CONFIG],
-        )
-    )
-
-    eval_configs = df[COL_EVAL_CONFIG].unique()
-    df_by_eval = []
-    for eval_config in eval_configs:
-        df_matched = df[df[COL_EVAL_CONFIG] == eval_config if eval_config is not None else df[COL_EVAL_CONFIG].isnull()]
-        df_by_eval.append(
-            (
-                json.loads(eval_config) if eval_config is not None else None,
-                _to_deserialized_dataframe(df_matched, column=COL_DATAPOINT),
-                _to_deserialized_dataframe(df_matched, column=COL_INFERENCE),
-                _to_deserialized_dataframe(df_matched, column=COL_METRICS),
-            ),
-        )
-
-    return df_by_eval
-
-
-def validate_data(left: pd.DataFrame, right: pd.DataFrame) -> None:
+def _validate_data(left: pd.DataFrame, right: pd.DataFrame) -> None:
     if len(left) != len(right):
         raise IncorrectUsageError("numbers of rows between two dataframe do not match")
 
@@ -291,56 +152,6 @@ def _validate_on(left: pd.DataFrame, right: pd.DataFrame, on: TEST_ON_TYPE) -> N
                 raise IncorrectUsageError(f"column {col} doesn't exist in target dataframe")
 
 
-def _get_default_infer_func(df_inf: pd.DataFrame, on: TEST_ON_TYPE) -> INFER_FUNC_TYPE:
-    if isinstance(on, str):
-        on = [on]
-
-    def infer(datapoints: pd.DataFrame) -> pd.DataFrame:
-        inferences = datapoints[on].merge(df_inf, how="left", on=on)
-        return inferences
-
-    return infer
-
-
-def _get_default_eval_func(df_metrics: pd.DataFrame, on: TEST_ON_TYPE) -> EVAL_FUNC_TYPE:
-    if isinstance(on, str):
-        on = [on]
-
-    def eval(
-        datapoints: pd.DataFrame,
-        inferences: pd.DataFrame,
-        eval_config: Optional[TYPE_EVALUATION_CONFIG],
-    ) -> pd.DataFrame:
-        metrics = datapoints[on].merge(df_metrics, how="left", on=on)
-        return metrics
-
-    return eval
-
-
-def _get_df_inferences(infer: TEST_INFER_TYPE, df_datapoints: pd.DataFrame, on: TEST_ON_TYPE) -> pd.DataFrame:
-    if isinstance(infer, pd.DataFrame):
-        _validate_on(df_datapoints, infer, on)
-        infer = _get_default_infer_func(infer, on)
-
-    df_inferences = infer(df_datapoints)
-    return df_inferences
-
-
-def _get_single_df_metrics(
-    eval: TEST_INFER_TYPE,
-    df_datapoints: pd.DataFrame,
-    df_inferences: pd.DataFrame,
-    config: Optional[TYPE_EVALUATION_CONFIG],
-    on: TEST_ON_TYPE,
-) -> pd.DataFrame:
-    if isinstance(eval, pd.DataFrame):
-        _validate_on(df_datapoints, eval, on)
-        eval = _get_default_eval_func(eval, on)
-
-    single_metrics = eval(df_datapoints, df_inferences, config)
-    return single_metrics
-
-
 def _get_single_df_result(
     df_datapoints: pd.DataFrame,
     df_result_input: pd.DataFrame,
@@ -348,6 +159,9 @@ def _get_single_df_result(
 ) -> pd.DataFrame:
     if not on:
         return df_result_input
+
+    if isinstance(on, str):
+        on = [on]
 
     _validate_on(df_datapoints, df_result_input, on)
     df_result = df_datapoints[on].merge(df_result_input, how="left", on=on)
@@ -382,10 +196,10 @@ def test(
     for config, df_result_input in results:
         log.info(f"start test with configuration {config}" if config else "start evaluation")
         single_result = _get_single_df_result(df_datapoints, df_result_input, on)
-        validate_data(df_datapoints, single_result)
+        _validate_data(df_datapoints, single_result)
         all_results.append((config, single_result))
         log.info(f"completed test with configuration {config}" if config else "completed evaluation")
 
-    df_results = _process_results(df_datapoints, all_results)
+    df_results = _process_results(df_data, all_results)
     n_uploaded_results = _upload_results(model, df_results)
     log.info(f"uploaded {n_uploaded_results} test results")
--- tests/integration/_experimental/dataset/test_evaluation.py
+++ tests/integration/_experimental/dataset/test_evaluation.py
@@ -20,13 +20,9 @@ import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
 
-from kolena._experimental.dataset import fetch_evaluation_results
-from kolena._experimental.dataset import fetch_inferences
+from kolena._experimental.dataset import fetch_results
 from kolena._experimental.dataset import register_dataset
 from kolena._experimental.dataset import test
-from kolena._experimental.dataset._evaluation import EVAL_FUNC_TYPE
-from kolena._experimental.dataset._evaluation import INFER_FUNC_TYPE
-from kolena._experimental.dataset._evaluation import TYPE_EVALUATION_CONFIG
 from kolena.errors import IncorrectUsageError
 from kolena.errors import NotFoundError
 from tests.integration.helper import fake_locator
@@ -55,50 +51,9 @@ def get_df_dp(n: int = 20) -> pd.DataFrame:
     return pd.DataFrame(records)
 
 
-def get_df_inf(n: int = 20) -> pd.DataFrame:
-    records = [dict(user_dp_id=i, softmax_bitmap=fake_locator(i, "softmax_bitmap")) for i in range(n)]
+def get_df_result(n: int = 20) -> pd.DataFrame:
+    records = [dict(user_dp_id=i, softmax_bitmap=fake_locator(i, "softmax_bitmap"), score=i * 0.1) for i in range(n)]
     return pd.DataFrame(records)
-
-
-def get_df_mtr(n: int = 20) -> pd.DataFrame:
-    records = [dict(user_dp_id=i, score=i) for i in range(n)]
-    return pd.DataFrame(records)
-
-
-def get_infer_func(
-    df_inf: pd.DataFrame,
-    columns: List[str],
-    id_col: str = "user_dp_id",
-    how: str = "left",
-    keep_none: bool = False,
-) -> INFER_FUNC_TYPE:
-    def infer_func(datapoints: pd.DataFrame) -> pd.DataFrame:
-        _inf = datapoints.set_index(id_col).join(df_inf.set_index(id_col), how=how).reset_index()[columns]
-        if keep_none:
-            _inf = _inf.replace({np.nan: None})
-        return _inf
-
-    return infer_func
-
-
-def get_eval_func(
-    df_mtr: pd.DataFrame,
-    columns: List[str],
-    id_col: str = "user_dp_id",
-    how: str = "left",
-    keep_none: bool = False,
-) -> EVAL_FUNC_TYPE:
-    def eval_func(
-        datapoints: pd.DataFrame,
-        inferences: pd.DataFrame,
-        eval_config: TYPE_EVALUATION_CONFIG,
-    ) -> pd.DataFrame:
-        _metrics = datapoints.set_index(id_col).join(df_mtr.set_index(id_col), how=how).reset_index()[columns]
-        if keep_none:
-            _metrics = _metrics.replace({np.nan: None})
-        return _metrics
-
-    return eval_func
 
 
 def test__test() -> None:
@@ -108,166 +63,65 @@ def test__test() -> None:
     dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
     register_dataset(dataset_name, df_dp[3:10][dp_columns])
 
-    df_inf = get_df_inf()
-    df_mtr = get_df_mtr()
-    inf_columns = ["softmax_bitmap"]
-    mtr_columns = ["score"]
-
-    eval_configs = [
-        {"threshold": 0.3},
-        {"threshold": 0.6},
-    ]
-    test(
-        dataset_name,
-        model_name,
-        infer=get_infer_func(df_inf, inf_columns),
-        eval=get_eval_func(df_mtr, mtr_columns),
-        eval_configs=eval_configs,
-    )
-
-    df_by_eval = fetch_evaluation_results(dataset_name, model_name)
-    assert len(df_by_eval) == 2
-    assert sorted([cfg for cfg, *_ in df_by_eval], key=lambda x: x.get("threshold")) == sorted(
-        eval_configs,
-        key=lambda x: x.get("threshold"),
-    )
-    expected_df_dp = df_dp[3:10].reset_index(drop=True)
-    expected_df_inf = df_inf[3:10].reset_index(drop=True)
-    expected_df_mtr = df_mtr[3:10].reset_index(drop=True)
-    for df_eval in df_by_eval:
-        _assert_frame_equal(df_eval[1], expected_df_dp, dp_columns)
-        _assert_frame_equal(df_eval[2], expected_df_inf, inf_columns)
-        _assert_frame_equal(df_eval[3], expected_df_mtr, mtr_columns)
-
-
-def test__test__df_for_infer_and_eval() -> None:
-    dataset_name = with_test_prefix(f"{__file__}::test__test__df_for_infer_and_eval")
-    model_name = with_test_prefix(f"{__file__}::test__test__df_for_infer_and_eval")
-    df_dp = get_df_dp()
-    dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
-    register_dataset(dataset_name, df_dp[3:10][dp_columns])
-
-    df_inf = get_df_inf()
-    df_mtr = get_df_mtr()
-    inf_columns = ["softmax_bitmap"]
-    mtr_columns = ["score"]
+    df_result = get_df_result()
+    result_columns = ["softmax_bitmap", "score"]
 
     test(
         dataset_name,
         model_name,
-        infer=df_inf,
-        eval=df_mtr,
+        df_result,
         on="user_dp_id",
     )
 
-    df_by_eval = fetch_evaluation_results(dataset_name, model_name)
-    assert len(df_by_eval) == 1
+    fetched_df_dp, df_results_by_eval = fetch_results(dataset_name, model_name)
+    eval_cfg, fetched_df_results = df_results_by_eval[0]
+    assert len(df_results_by_eval) == 1
+    assert eval_cfg is None
     expected_df_dp = df_dp[3:10].reset_index(drop=True)
-    expected_df_inf = df_inf[3:10].reset_index(drop=True)
-    expected_df_mtr = df_mtr[3:10].reset_index(drop=True)
-    for df_eval in df_by_eval:
-        _assert_frame_equal(df_eval[1], expected_df_dp, dp_columns)
-        _assert_frame_equal(df_eval[2], expected_df_inf, inf_columns)
-        _assert_frame_equal(df_eval[3], expected_df_mtr, mtr_columns)
+    expected_df_result = df_result[3:10].reset_index(drop=True)
+    _assert_frame_equal(fetched_df_dp, expected_df_dp, dp_columns)
+    _assert_frame_equal(fetched_df_results, expected_df_result, result_columns)
 
 
-def test__test__missing_inference() -> None:
-    dataset_name = with_test_prefix(f"{__file__}::test__test__missing_inference")
-    model_name = with_test_prefix(f"{__file__}::test__test__missing_inference")
+def test__test__missing_result() -> None:
+    dataset_name = with_test_prefix(f"{__file__}::test__test__missing_result")
+    model_name = with_test_prefix(f"{__file__}::test__test__missing_result")
     df_dp = get_df_dp()
     dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
     register_dataset(dataset_name, df_dp[3:10][dp_columns])
 
-    df_inf = get_df_inf(5)
-    inf_columns = ["softmax_bitmap"]
+    df_result = get_df_result()
+    result_columns = ["softmax_bitmap", "score"]
 
     test(
         dataset_name,
         model_name,
-        infer=get_infer_func(df_inf, inf_columns),
+        df_result,
+        on="user_dp_id",
     )
 
-    df_datapoints, df_inferences = fetch_inferences(dataset_name, model_name)
+    fetched_df_dp, df_results_by_eval = fetch_results(dataset_name, model_name)
+    eval_cfg, fetched_df_results = df_results_by_eval[0]
+    assert len(df_results_by_eval) == 1
+    assert eval_cfg is None
     expected_df_dp = df_dp[3:10].reset_index(drop=True)
-    expected_df_inf = pd.concat([df_inf[3:5], pd.DataFrame({"softmax_bitmap": [np.nan] * 5})], axis=0).reset_index(
-        drop=True,
-    )
-    _assert_frame_equal(df_datapoints, expected_df_dp, dp_columns)
-    _assert_frame_equal(df_inferences, expected_df_inf, inf_columns)
+    expected_df_result = df_result[3:10].reset_index(drop=True)
+    _assert_frame_equal(fetched_df_dp, expected_df_dp, dp_columns)
+    _assert_frame_equal(fetched_df_results, expected_df_result, result_columns)
 
-    # add 3 new datapoints, then we should have missing inference in the db records
+    # add 3 new datapoints, then we should have missing results in the db records
     register_dataset(dataset_name, df_dp[:10][dp_columns])
-    df_datapoints, df_inferences = fetch_inferences(dataset_name, model_name)
+    fetched_df_dp, df_results_by_eval = fetch_results(dataset_name, model_name)
+    eval_cfg, fetched_df_results = df_results_by_eval[0]
+    assert len(df_results_by_eval) == 1
+    assert eval_cfg is None
     expected_df_dp = pd.concat([df_dp[3:10], df_dp[:3]]).reset_index(drop=True)
-    expected_df_inf = pd.concat(
-        [df_inf[3:5], pd.DataFrame({"softmax_bitmap": [np.nan] * (5 + 3)})],
+    expected_df_result = pd.concat(
+        [df_result[3:10], pd.DataFrame({"softmax_bitmap": [np.nan] * 3, "score": [np.nan] * 3})],
         axis=0,
     ).reset_index(drop=True)
-    _assert_frame_equal(df_datapoints, expected_df_dp, dp_columns)
-    _assert_frame_equal(df_inferences, expected_df_inf, inf_columns)
-
-
-def test__test__missing_metrics() -> None:
-    dataset_name = with_test_prefix(f"{__file__}::test__test__missing_metrics")
-    model_name = with_test_prefix(f"{__file__}::test__test__missing_metrics")
-    df_dp = get_df_dp()
-    dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
-    register_dataset(dataset_name, df_dp[13:20][dp_columns])
-
-    df_inf = get_df_inf()
-    df_mtr = get_df_mtr()
-    inf_columns = ["softmax_bitmap"]
-    mtr_columns = ["score"]
-
-    test(
-        dataset_name,
-        model_name,
-        infer=get_infer_func(df_inf, inf_columns),
-        eval=get_eval_func(df_mtr, mtr_columns),
-    )
-
-    df_by_eval = fetch_evaluation_results(dataset_name, model_name)
-    eval_cfg, df_datapoints, df_inferences, df_metrics = df_by_eval[0]
-    expected_df_dp = df_dp[13:20].reset_index(drop=True)
-    expected_df_inf = pd.concat([df_inf[13:20]], axis=0).reset_index(
-        drop=True,
-    )
-    expected_df_mtr = pd.concat([df_mtr[13:20]], axis=0).reset_index(
-        drop=True,
-    )
-    assert len(df_by_eval) == 1
-    assert eval_cfg is None
-    _assert_frame_equal(df_datapoints, expected_df_dp, dp_columns)
-    _assert_frame_equal(df_inferences, expected_df_inf, inf_columns)
-    _assert_frame_equal(df_metrics, expected_df_mtr, mtr_columns)
-
-    # add 3 new datapoints, then we should have missing inference and metrics in the db records
-    register_dataset(dataset_name, df_dp[10:20][dp_columns])
-
-    df_by_eval = fetch_evaluation_results(dataset_name, model_name)
-    eval_cfg, df_datapoints, df_inferences, df_metrics = df_by_eval[0]
-    expected_df_dp = pd.concat([df_dp[13:20], df_dp[10:13]]).reset_index(drop=True)
-    expected_df_inf = pd.concat(
-        [df_inf[13:20], pd.DataFrame({"softmax_bitmap": [np.nan] * 3})],
-        axis=0,
-    ).reset_index(drop=True)
-    expected_df_mtr = pd.concat([df_mtr[13:20], pd.DataFrame({"score": [np.nan] * 3})], axis=0).reset_index(
-        drop=True,
-    )
-    assert len(df_by_eval) == 1
-    assert eval_cfg is None
-    _assert_frame_equal(df_datapoints, expected_df_dp, dp_columns)
-    _assert_frame_equal(df_inferences, expected_df_inf, inf_columns)
-    _assert_frame_equal(df_metrics, expected_df_mtr, mtr_columns)
-
-    with pytest.raises(IncorrectUsageError) as exc_info:
-        test(
-            dataset_name,
-            model_name,
-            eval=get_eval_func(df_mtr, mtr_columns),
-        )
-    exc_info_value = str(exc_info.value)
-    assert "cannot upload metrics without inference" in exc_info_value
+    _assert_frame_equal(fetched_df_dp, expected_df_dp, dp_columns)
+    _assert_frame_equal(fetched_df_results, expected_df_result, result_columns)
 
 
 def test__test__upload_none() -> None:
@@ -277,52 +131,29 @@ def test__test__upload_none() -> None:
     dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
     register_dataset(dataset_name, df_dp[dp_columns])
 
-    df_inf = get_df_inf(10)
-    df_mtr = get_df_mtr(10)
-    inf_columns = ["softmax_bitmap"]
-    mtr_columns = ["score"]
+    df_result = get_df_result(10)
+    result_columns = ["softmax_bitmap", "score"]
 
     test(
         dataset_name,
         model_name,
-        infer=get_infer_func(df_inf, inf_columns, keep_none=True),
-        eval=get_eval_func(df_mtr, mtr_columns, keep_none=True),
+        df_result,
+        on=["user_dp_id"],
     )
 
-    df_by_eval = fetch_evaluation_results(dataset_name, model_name)
-    eval_cfg, df_datapoints, df_inferences, df_metrics = df_by_eval[0]
+    fetched_df_dp, df_results_by_eval = fetch_results(dataset_name, model_name)
+    eval_cfg, fetched_df_results = df_results_by_eval[0]
     expected_df_dp = df_dp.reset_index(drop=True)
-    expected_df_inf = pd.concat([df_inf, pd.DataFrame({"softmax_bitmap": [np.nan] * 10})], axis=0).reset_index(
+    expected_df_result = pd.concat(
+        [df_result, pd.DataFrame({"softmax_bitmap": [np.nan] * 10, "score": [np.nan] * 10})],
+        axis=0,
+    ).reset_index(
         drop=True,
     )
-    expected_df_mtr = pd.concat([df_mtr, pd.DataFrame({"score": [np.nan] * 10})], axis=0).reset_index(
-        drop=True,
-    )
-    assert len(df_by_eval) == 1
+    assert len(df_results_by_eval) == 1
     assert eval_cfg is None
-    _assert_frame_equal(df_datapoints, expected_df_dp, dp_columns)
-    _assert_frame_equal(df_inferences, expected_df_inf, inf_columns)
-    _assert_frame_equal(df_metrics, expected_df_mtr, mtr_columns)
-
-
-def test__test__invalid_data__eval_before_inf() -> None:
-    dataset_name = with_test_prefix(f"{__file__}::test__test__invalid_data__eval_before_inf")
-    model_name = with_test_prefix(f"{__file__}::test__test__invalid_data__eval_before_inf")
-    df_dp = get_df_dp()
-    dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
-    register_dataset(dataset_name, df_dp[13:20][dp_columns])
-
-    df_mtr = get_df_mtr()
-    mtr_columns = ["score"]
-
-    with pytest.raises(IncorrectUsageError) as exc_info:
-        test(
-            dataset_name,
-            model_name,
-            eval=get_eval_func(df_mtr, mtr_columns),
-        )
-    exc_info_value = str(exc_info.value)
-    assert "cannot upload metrics without inference" in exc_info_value
+    _assert_frame_equal(fetched_df_dp, expected_df_dp, dp_columns)
+    _assert_frame_equal(fetched_df_results, expected_df_result, result_columns)
 
 
 def test__test__invalid_data__df_size_mismatch() -> None:
@@ -332,40 +163,22 @@ def test__test__invalid_data__df_size_mismatch() -> None:
     dp_columns = ["user_dp_id", "locator", "width", "height", "city"]
     register_dataset(dataset_name, df_dp[dp_columns])
 
-    df_inf = get_df_inf(10)
-    df_mtr = get_df_mtr(10)
-    inf_columns = ["softmax_bitmap"]
-    mtr_columns = ["score"]
+    df_result = get_df_result(10)
 
     with pytest.raises(IncorrectUsageError) as exc_info:
         test(
             dataset_name,
             model_name,
-            infer=get_infer_func(df_inf, inf_columns, how="inner"),
-        )
-    exc_info_value = str(exc_info.value)
-    assert "numbers of rows between two dataframe do not match" in exc_info_value
-
-    with pytest.raises(IncorrectUsageError) as exc_info:
-        test(
-            dataset_name,
-            model_name,
-            eval=get_eval_func(df_mtr, mtr_columns, how="inner"),
+            df_result,
         )
     exc_info_value = str(exc_info.value)
     assert "numbers of rows between two dataframe do not match" in exc_info_value
 
 
-def test__fetch_inferences__not_exist() -> None:
-    dataset_name = with_test_prefix(f"{__file__}::test__fetch_inferences__not_exist")
-    model_name = with_test_prefix(f"{__file__}::test__fetch_inferences__not_exist")
-    df_datapoints, df_inferences = fetch_inferences(dataset_name, model_name)
-    assert df_datapoints.empty
-    assert df_inferences.empty
-
-
-def test__fetch_evaluation_results__not_exist() -> None:
-    dataset_name = with_test_prefix(f"{__file__}::test__fetch_evaluation_results__not_exist")
-    model_name = with_test_prefix(f"{__file__}::test__fetch_evaluation_results__not_exist")
-    with pytest.raises(NotFoundError):
-        fetch_evaluation_results(dataset_name, model_name)
+def test__fetch_results__not_exist() -> None:
+    dataset_name = with_test_prefix(f"{__file__}::test__fetch_results__not_exist")
+    model_name = with_test_prefix(f"{__file__}::test__fetch_results__not_exist")
+    with pytest.raises(NotFoundError) as exc_info:
+        fetch_results(dataset_name, model_name)
+    exc_info_value = str(exc_info.value)
+    assert "no such model" in exc_info_value
