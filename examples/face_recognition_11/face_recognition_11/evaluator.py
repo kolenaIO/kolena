ce00b1ba526de380221efb141850ba0aef7d8d20
Yoohee Choi <yoohee@kolena.io>
2023-11-09 14:02:30 -0500

    Fixed thresholds in plot

--- examples/face_recognition_11/face_recognition_11/evaluator.py
+++ examples/face_recognition_11/face_recognition_11/evaluator.py
@@ -299,7 +299,8 @@ def compute_test_case_plots(
     fmr_y = list()
 
     baseline_fmr_x = [baseline_fmr for baseline_fmr, _ in baseline_thresholds]
-    for _, threshold in baseline_thresholds:
+    thresholds = [threshold for _, threshold in baseline_thresholds]
+    for _, threshold in thresholds:
         tsm_for_one_threshold = [
             compute_per_sample(gt, inf, threshold, configuration) for gt, inf in zip(ground_truths, inferences)
         ]
@@ -318,7 +319,7 @@ def compute_test_case_plots(
             x_label="Baseline False Match Rate",
             y_label="Test Case False Non-Match Rate (%)",
             x_config=AxisConfig(type="log"),
-            curves=[Curve(x=baseline_fmr_x, y=fnmr_y, extra=dict(Threshold=baseline_thresholds))],
+            curves=[Curve(x=baseline_fmr_x, y=fnmr_y, extra=dict(Threshold=thresholds))],
         ),
     )
 
@@ -329,7 +330,7 @@ def compute_test_case_plots(
             y_label="Test Case False Match Rate",
             x_config=AxisConfig(type="log"),
             y_config=AxisConfig(type="log"),
-            curves=[Curve(x=baseline_fmr_x, y=fmr_y, extra=dict(Threshold=baseline_thresholds))],
+            curves=[Curve(x=baseline_fmr_x, y=fmr_y, extra=dict(Threshold=thresholds))],
         ),
     )
 
