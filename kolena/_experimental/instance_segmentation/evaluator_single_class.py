0ab405e8a02b2f8b7503400b63e44c82f8c4e1de
Connor Soulliere <connor@kolena.io>
2023-12-08 12:40:03 -0500

    Add Instance Segmentation workflow wrapper on Object Detection
    Adds an experimental Instance Segmentation workflow which delegates to
    prebuilt Object Detection workflow. The Object Detection prebulit is
    capable of handling both bounding boxes and polygons, however it is
    unintuitive that a Object Detection workflow be used for polygons.
    This commit adds a thin wrapper on Object Detection to properly
    classify the workflow as "Instance Segmentation".

---
+++ kolena/_experimental/instance_segmentation/evaluator_single_class.py
@@ -0,0 +1,27 @@
+# Copyright 2021-2023 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from kolena._experimental.object_detection.evaluator_single_class import SingleClassObjectDetectionEvaluator
+
+
+class SingleClassInstanceSegmentationEvaluator(SingleClassObjectDetectionEvaluator):
+    """
+    The `SingleClassInstanceSegmentationEvaluator` transforms inferences into metrics for
+    the Instance Segmentation workflow for a single class.
+
+    When a [`ThresholdConfiguration`][kolena._experimental.object_detection.workflow.ThresholdConfiguration] is
+    configured to use an F1-Optimal threshold strategy, the evaluator requires that the first test case retrieved for
+    a test suite contains the complete sample set.
+
+    For additional functionality, see the associated [base class documentation][kolena.workflow.evaluator.Evaluator].
+    """
