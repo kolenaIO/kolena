197201d5697776b256606e60d65340871f465ecb
Grant <grant@kolena.io>
2024-01-03 08:40:23 -0800

    only run experimental once

--- kolena/dataset/__init__.py
+++ kolena/dataset/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 Kolena Inc.
+# Copyright 2021-2024 Kolena Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,12 +10,14 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License
-from kolena.dataset.dataset import register_dataset, fetch_dataset_history, fetch_dataset
-from kolena.dataset.evaluation import fetch_results, test
+# limitations under the License.
 # noreorder
-from kolena.workflow import annotation
-from kolena.workflow import asset
+from kolena.dataset.dataset import fetch_dataset_history
+from kolena.dataset.dataset import fetch_dataset
+from kolena.dataset.dataset import register_dataset
+from kolena.dataset.evaluation import fetch_results
+from kolena.dataset.evaluation import test
+from kolena.workflow import annotation, asset
 
 __all__ = [
     "register_dataset",
@@ -26,8 +28,3 @@ __all__ = [
     "annotation",
     "asset",
 ]
-
-# noreorder
-from kolena.workflow import annotation
-from kolena.workflow import asset
-
