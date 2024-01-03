434882afeac32e903c9a86a6fe23162b63366eea
Yifan Wu <yifan@kolena.io>
2024-01-03 10:32:00 -0800

    chore: fix tests

--- tests/unit/dataset/test_annotation.py
+++ tests/unit/dataset/test_annotation.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from kolena.dataset.annotation import Annotation
 
 
 def test__import():
-    Annotation
+    from kolena.dataset.annotation import BoundingBox
+
+    BoundingBox
--- tests/unit/dataset/test_asset.py
+++ tests/unit/dataset/test_asset.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from kolena.dataset.asset import Asset
 
 
 def test__import():
-    Asset
+    from kolena.dataset.asset import ImageAsset
+
+    ImageAsset
