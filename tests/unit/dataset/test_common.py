4529b2d9697bd944102e79ab2e2800fa4680b8c1
Connor Soulliere <connor@kolena.io>
2024-01-04 08:15:04 -0800

    Update unit test

--- tests/unit/dataset/test_common.py
+++ tests/unit/dataset/test_common.py
@@ -70,12 +70,11 @@ def test__validate_dataframe_ids(df: pd.DataFrame, id_fields: List[str]) -> None
     [
         # dataframe is missing one of id_fields
         (pd.DataFrame(dict(a=[1, 2, 3])), ["a", "b"]),
-        # dataframe id_fields datatypes are not primitives
+        # dataframe id_fields are not hashable
         (pd.DataFrame(dict(a=[dict(c=i) for i in range(3)], b=[1, 2, 1])), ["a", "b"]),
         (pd.DataFrame(dict(a=[[1], [2], [3]], b=[1, 2, 1])), ["a", "b"]),
         # dataframe values in id_fields is not unique
         (pd.DataFrame(dict(a=[1, 2, 1], b=[1, 2, 1])), ["a", "b"]),
-        (pd.DataFrame(dict(a=[1, 1, 1], b=[1, 2, 1])), ["a", "b"]),
         # the key sequence difference will not make it unique
         (pd.DataFrame(dict(a=[{"a": 1, "b": 2}, {"a": 2, "b": 1}, {"b": 2, "a": 1}], b=[1, 2, 1])), ["a", "b"]),
         (
