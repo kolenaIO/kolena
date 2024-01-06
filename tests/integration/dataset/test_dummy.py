854da2cc6f5297f66959ca16002641c649b1639b
Yifan Wu <yifan@kolena.io>
2024-01-05 16:10:07 -0800

    chore: clean up

--- tests/integration/dataset/test_dummy.py
+++ tests/integration/dataset/test_dummy.py
@@ -11,6 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# dummy test to bypass the parallelism
 def test__dummy() -> None:
+    # dummy test to bypass the parallelism
     ...
