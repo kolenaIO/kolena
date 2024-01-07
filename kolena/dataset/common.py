ea2a79d1dd35f20704b1d7af1ca5ef5e669ec0be
Connor Soulliere <connor@kolena.io>
2024-01-07 13:01:34 -0500

    Enforce id fields to be hashable
    During stress testing, the id_field uniqueness check was found to be a
    significant bottleneck. The cause was converting all id_fields to a
    dict to compare objects by value.
    
    This commit addresses the issue by enforcing id_fields to be
    hashable so the conversion to dicts is not necessary. This speeds up
    id_field validation checks.

--- kolena/dataset/common.py
+++ kolena/dataset/common.py
@@ -47,10 +47,13 @@ def validate_id_fields(id_fields: List[str], existing_id_fields: Optional[List[s
 
 
 def _validate_dataframe_ids_uniqueness(df: pd.DataFrame, id_fields: List[str]) -> None:
-    if df[id_fields].apply(lambda x: x.to_dict(), axis=1).duplicated().any():
-        raise InputValidationError(
-            f"invalid id_fields: " f"input dataframe's id field values are not unique for {id_fields}",
-        )
+    try:
+        if df[id_fields].duplicated().any():
+            raise InputValidationError(
+                f"invalid id_fields: " f"input dataframe's id field values are not unique for {id_fields}",
+            )
+    except TypeError as e:
+        raise InputValidationError("id fields must be hashable: ", e)
 
 
 def validate_dataframe_ids(df: pd.DataFrame, id_fields: List[str]) -> None:
