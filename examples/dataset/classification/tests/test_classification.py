e534f274d2cad64dd3bfc21f2e39dec031aecd41
Mark Chen <markzchen1@gmail.com>
2024-01-07 22:02:49 -0500

    combine tests

--- examples/dataset/classification/tests/test_classification.py
+++ examples/dataset/classification/tests/test_classification.py
@@ -18,13 +18,16 @@ from argparse import Namespace
 from collections.abc import Iterator
 
 import pytest
-from classification.binary.upload_dataset import run as upload_binary_dataset_main
-from classification.binary.upload_results import run as upload_binary_results_main
+from classification.binary.upload_dataset import run as upload_binary_dataset
+from classification.binary.upload_results import run as upload_binary_results
+from classification.multiclass.upload_dataset import run as upload_multiclass_dataset
+from classification.multiclass.upload_results import run as upload_multiclass_results
 
 from kolena._utils.state import kolena_session
 
 BUCKET = "kolena-public-examples"
-DATASET = "dogs-vs-cats"
+BINARY_DATASET = "dogs-vs-cats"
+MULTICLASS_DATASET = "cifar10"
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -34,19 +37,21 @@ def with_init() -> Iterator[None]:
 
 
 @pytest.fixture(scope="module")
-def binary_dataset_name() -> str:
+def random_prefix() -> str:
     TEST_PREFIX = "".join(random.choices(string.ascii_uppercase + string.digits, k=12))
-    return f"{TEST_PREFIX} - {DATASET}"
+    return TEST_PREFIX
 
 
-def test__upload_binary_dataset(binary_dataset_name: str) -> None:
-    args = Namespace(dataset=binary_dataset_name)
-    upload_binary_dataset_main(args)
+def test__upload_dataset(random_prefix: str) -> None:
+    args = Namespace(dataset=random_prefix + f" - {BINARY_DATASET}")
+    upload_binary_dataset(args)
+    args = Namespace(dataset=random_prefix + f" - {MULTICLASS_DATASET}")
+    upload_multiclass_dataset(args)
 
 
-@pytest.mark.depends(on=["test__upload_binary_dataset"])
-def test__upload_results(binary_dataset_name: str) -> None:
-    args = Namespace(model="resnet50v2", dataset=binary_dataset_name)
-    upload_binary_results_main(args)
-    args = Namespace(model="inceptionv3", dataset=binary_dataset_name)
-    upload_binary_results_main(args)
+@pytest.mark.depends(on=["test__upload_dataset"])
+def test__upload_results(random_prefix: str) -> None:
+    args = Namespace(model="inceptionv3", dataset=random_prefix + f" - {BINARY_DATASET}")
+    upload_binary_results(args)
+    args = Namespace(model="resnet50v2", dataset=random_prefix + f" - {MULTICLASS_DATASET}")
+    upload_multiclass_results(args)
