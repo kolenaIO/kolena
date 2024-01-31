4726f3c2fe794d43f435a11ca956aca7e25976bc
Gordon Hart <gordon.hart2@gmail.com>
2024-01-30 19:34:43 -0500

    Fall back to importing Annotated from typing_extensions

--- kolena/workflow/_validators.py
+++ kolena/workflow/_validators.py
@@ -20,6 +20,11 @@ from typing import Optional
 from typing import Type
 from typing import Union
 
+try:
+    from typing import Annotated  # type: ignore
+except ImportError:
+    from typing_extensions import Annotated  # type: ignore
+
 from kolena._experimental.workflow.thresholded import ThresholdedMetrics
 from kolena._utils.datatypes import DATA_TYPE_FIELD
 from kolena._utils.datatypes import DataObject
@@ -132,7 +137,7 @@ def validate_union(
         if arg_origin is list or arg_origin is List:
             validate_list(field_name, arg, supported_list_types)
             continue
-        if arg_origin is typing.Annotated:  # used by Pydantic StrictBool, StrictStr, etc.
+        if arg_origin is Annotated:  # used by Pydantic StrictBool, StrictStr, etc.
             annotated_type, *_ = get_args(arg)
             if not issubclass(annotated_type, tuple(supported_field_types)):
                 raise ValueError(err)
