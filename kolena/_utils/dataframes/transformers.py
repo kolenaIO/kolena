d50497aff7b4cf3d40190df61197aa1c7d5b2a30
Yifan Wu <yifan@kolena.io>
2024-02-02 12:00:03 -0500

    chore: add more tests

--- kolena/_utils/dataframes/transformers.py
+++ kolena/_utils/dataframes/transformers.py
@@ -57,7 +57,7 @@ def _normalize_dicts(dict_list: List[Dict[str, Any]], sep: str = ".", max_level:
 
 def json_normalize(data: List[Dict[str, Any]], max_level: int = 0) -> pd.DataFrame:
     """
-    pd.json_normalize wrapper
+    hand roll pandas.json_normalize implementation
     use None for missing value
     """
     normalized_data = _normalize_dicts(data, max_level=max_level)
