98e236f1d20d695cb5eb0dba0a6fe2a64de41c6e
Saad Hossain <68381793+sdhossain@users.noreply.github.com>
2024-02-23 15:59:48 -0500

    Semantic Textual Similarity Example (#519)
    * added example of semantic textual similarity - first iteration
    
    * first iteration after PR - rmeoved test anfd updated README
    
    ---------
    
    Co-authored-by: Saad Hossain <saadhossain@Mac.local>

---
+++ examples/dataset/semantic_textual_similarity/tests/test_semantic_textual_similarity.py
@@ -0,0 +1,39 @@
+# Copyright 2021-2024 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import random
+import string
+from argparse import Namespace
+
+import pytest
+from semantic_textual_similarity.constants import BUCKET
+from semantic_textual_similarity.constants import DATASET
+from semantic_textual_similarity.upload_dataset import run as upload_dataset_run
+from semantic_textual_similarity.upload_results import run as upload_results_run
+
+
+@pytest.fixture(scope="module")
+def dataset_name() -> str:
+    TEST_PREFIX = "".join(random.choices(string.ascii_uppercase + string.digits, k=12))
+    return f"{TEST_PREFIX} - {DATASET}"
+
+
+def test__upload_dataset__smoke(dataset_name: str) -> None:
+    args = Namespace(dataset_csv=f"s3://{BUCKET}/{DATASET}/sts-benchmark.csv", dataset=dataset_name)
+    upload_dataset_run(args)
+
+
+@pytest.mark.depends(on=["test__upload_dataset__smoke"])
+def test__upload_results__smoke(dataset_name: str) -> None:
+    args = Namespace(model="distilroberta", dataset=dataset_name)
+    upload_results_run(args)
