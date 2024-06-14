a692ba8a0407dc3e21122ed025025855d6065a84
Andrew Shi <munkyshi22@gmail.com>
2024-06-14 13:05:20 -0400

    Remove detection and alignment metrics

--- examples/dataset/face_recognition_11/face_recognition_11/metrics.py
+++ examples/dataset/face_recognition_11/face_recognition_11/metrics.py
@@ -11,87 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import math
 from typing import Any
 from typing import Dict
-from typing import List
-from typing import Optional
-from typing import Tuple
 
-import numpy as np
 import pandas as pd
-from face_recognition_11.image import FRImageAsset
-
-from kolena.annotation import BoundingBox
-from kolena.annotation import Keypoints
-from kolena.workflow.metrics import iou
-
-
-def compute_distance(point_a: Tuple[float, float], point_b: Tuple[float, float]) -> float:
-    return math.sqrt(math.pow(point_a[0] - point_b[0], 2) + math.pow(point_a[1] - point_b[1], 2))
-
-
-def compute_detection_metrics(
-    gt: BoundingBox,
-    inf: Optional[BoundingBox],
-    configuration: Dict[str, Any],
-) -> Dict[str, Any]:
-    if inf is None:
-        return dict(
-            detection_TP=[],
-            detection_FP=[],
-            detection_FN=[gt],
-            detection_is_TP=False,
-            detection_is_FP=False,
-            detection_is_FN=True,
-            detection_failure=True,
-            detection_threshold=configuration["iou_threshold"],
-        )
-
-    iou_value = iou(gt, inf)
-    detected = iou_value >= configuration["iou_threshold"]
-
-    return dict(
-        detection_TP=[inf] if detected else [],
-        detection_FP=[inf] if not detected else [],
-        detection_FN=[gt] if not detected else [],
-        detection_is_TP=detected,
-        detection_is_FP=not detected,
-        detection_is_FN=not detected,
-        detection_failure=not detected,
-        detection_iou=iou_value,
-        detection_threshold=configuration["iou_threshold"],
-    )
-
-
-def compute_alignment_metrics(
-    norm_factor: float,
-    gt: Keypoints,
-    inf: Keypoints,
-    configuration: Dict[str, Any],
-) -> Dict[str, Any]:
-    if len(inf.points) == 0:
-        return dict(
-            alignment_failure=True,
-            alignment_threshold=configuration["nrmse_threshold"],
-        )
-
-    Δ_left_eye = compute_distance(gt.points[0], inf.points[0])
-    Δ_right_eye = compute_distance(gt.points[1], inf.points[1])
-    distances = np.array([Δ_left_eye, Δ_right_eye])
-    mse = np.mean(distances**2)
-    nrmse = math.sqrt(np.mean((distances / norm_factor) ** 2))
-
-    return dict(
-        alignment_MSE=mse,
-        alignment_NRMSE=nrmse,
-        alignment_Δ_left_eye=Δ_left_eye,
-        alignment_Δ_right_eye=Δ_right_eye,
-        alignment_norm_Δ_left_eye=Δ_left_eye / norm_factor,
-        alignment_norm_Δ_right_eye=Δ_right_eye / norm_factor,
-        alignment_failure=nrmse >= configuration["nrmse_threshold"],
-        alignment_threshold=configuration["nrmse_threshold"],
-    )
 
 
 def compute_recognition_threshold(df_pair_results: pd.DataFrame, fmr: float, eps: float = 1e-9) -> float:
@@ -118,18 +41,4 @@ def compute_pairwise_recognition_metrics(is_match: bool, similarity: float, thre
         is_FM=not is_match and predicted_match,
         FMR=None if is_match else 1 if predicted_match else 0,
         FNMR=None if not is_match else 0 if predicted_match else 1,
-    )
-
-
-def compute_recognition_merics(pairs: List[FRImageAsset], threshold: float) -> Dict[str, Any]:
-    return dict(
-        recognition_count_TM=sum(pair.is_TM for pair in pairs if pair.is_TM is not None),
-        recognition_count_TNM=sum(pair.is_TNM for pair in pairs if pair.is_TNM is not None),
-        recognition_count_FM=sum(pair.is_FM for pair in pairs if pair.is_FM is not None),
-        recognition_count_FNM=sum(pair.is_FNM for pair in pairs if pair.is_FNM is not None),
-        recognition_FMR=sum(pair.is_FM for pair in pairs if pair.is_FM is not None),
-        recognition_FNMR=sum(pair.is_FNM for pair in pairs if pair.is_FNM is not None),
-        recognition_threshold=threshold,
-        recognition_genuine_similarity=np.mean([pair.similarity for pair in pairs if pair.is_match]),
-        recognition_imposter_similarity=np.mean([pair.similarity for pair in pairs if not pair.is_match]),
     )
