1d7e26354f841377c36953d21e629f6d382c967b
Andrew Shi <andrew@kolena.io>
2024-06-17 20:38:11 -0400

    Add Face Recognition 1:1 Example (#618)
    * Added a new example for FR 1:1 for Kolena Datasets
    
    * Read eval config from config json file
    
    * Updated WORKFLOW to TASK
    
    * Checkpoint: changed structure to table with pairs as datapoints
    
    * Push results at different target false match rates
    
    * Fix type checks
    
    * Include standalone assets for display in table
    
    * Remove detection and alignment metrics
    
    * Clean up pairwise recognition metrics
    
    Add threshold, remove is_failure
    
    * Clean up README
    
    * Fix up some column naming
    
    * Limit numpy dependency for examples
    
    ---------
    
    Co-authored-by: Yoohee Choi <yoohee@kolena.io>

---
+++ examples/dataset/face_recognition_11/face_recognition_11/metrics.py
@@ -0,0 +1,44 @@
+# Copyright 2021-2024 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any
+from typing import Dict
+
+import pandas as pd
+
+
+def compute_recognition_threshold(df_pair_results: pd.DataFrame, fmr: float, eps: float = 1e-9) -> float:
+    imposter_scores = sorted(
+        [
+            pair.similarity if pair.similarity is not None else 0.0
+            for pair in df_pair_results.itertuples()
+            if not pair.is_match
+        ],
+        reverse=True,
+    )
+    threshold_idx = int(round(fmr * len(imposter_scores) / 2) - 1)
+    threshold = imposter_scores[threshold_idx * 2] - eps
+    return threshold
+
+
+def compute_pairwise_recognition_metrics(is_match: bool, similarity: float, threshold: float) -> Dict[str, Any]:
+    predicted_match = similarity > threshold
+    return dict(
+        threshold=threshold,
+        is_TM=is_match and predicted_match,
+        is_TNM=not is_match and not predicted_match,
+        is_FNM=is_match and not predicted_match,
+        is_FM=not is_match and predicted_match,
+        FMR=None if is_match else 1 if predicted_match else 0,
+        FNMR=None if not is_match else 0 if predicted_match else 1,
+    )
---
+++ examples/dataset/face_recognition_11/face_recognition_11/upload_dataset.py
@@ -0,0 +1,106 @@
+# Copyright 2021-2024 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from argparse import ArgumentParser
+from argparse import Namespace
+
+import pandas as pd
+from face_recognition_11.constants import DATASET
+from face_recognition_11.constants import DATASET_DETECTION
+from face_recognition_11.constants import DATASET_METADATA
+from face_recognition_11.constants import DATASET_PAIRS
+from face_recognition_11.constants import TASK
+
+from kolena.annotation import BoundingBox
+from kolena.annotation import Keypoints
+from kolena.asset import ImageAsset
+from kolena.dataset import upload_dataset
+
+
+def run(args: Namespace) -> None:
+    df_metadata = pd.read_csv(DATASET_METADATA)
+    df_pairs = pd.read_csv(DATASET_PAIRS)
+    df_detection = pd.read_csv(DATASET_DETECTION)
+
+    genuine_pairs = {}
+    imposter_pairs = {}
+    for record in df_pairs.itertuples():
+        if record.is_match:
+            genuine_pairs[record.locator_left] = record.locator_right
+        else:
+            imposter_pairs[record.locator_left] = record.locator_right
+
+    df_raw_data = df_detection.merge(df_metadata, on="locator", how="left")
+    df_raw_data["image_asset"] = df_raw_data.apply(
+        lambda x: ImageAsset(
+            locator=x.locator,
+            width=x.width,  # type: ignore[call-arg]
+            height=x.height,  # type: ignore[call-arg]
+            normalization_factor=x.normalization_factor,  # type: ignore[call-arg]
+            bbox=BoundingBox(
+                top_left=(x.min_x, x.min_y),
+                bottom_right=(
+                    x.max_x,
+                    x.max_y,
+                ),
+            ),  # type: ignore[call-arg]
+            keypoints=Keypoints(
+                points=[
+                    (x.left_eye_x, x.left_eye_y),
+                    (x.right_eye_x, x.right_eye_y),
+                ],
+            ),  # type: ignore[call-arg]
+        ),
+        axis=1,
+    )
+    df_raw_data["person"] = df_raw_data.apply(
+        lambda x: dict(
+            person=x.person,
+            age=x.age,
+            race=x.race,
+            gender=x.gender,
+        ),
+        axis=1,
+    )
+    df_raw_data = df_raw_data[["locator", "image_asset", "person"]]
+    df_pairs = df_pairs.merge(df_raw_data, left_on="locator_left", right_on="locator")
+    df_pairs = df_pairs.merge(df_raw_data, left_on="locator_right", right_on="locator")
+    df_pairs["image_left"] = df_pairs["image_asset_x"].apply(
+        lambda image: ImageAsset(**image.__dict__, position="left"),  # type: ignore[call-arg]
+    )
+    df_pairs["image_right"] = df_pairs["image_asset_y"].apply(
+        lambda image: ImageAsset(**image.__dict__, position="right"),  # type: ignore[call-arg]
+    )
+    df_pairs["pairs"] = df_pairs.apply(lambda x: [x["image_left"], x["image_right"]], axis=1)
+
+    df_pairs = df_pairs.rename(columns={"person_x": "left", "person_y": "right"})
+    df_pairs = df_pairs[
+        ["locator_left", "locator_right", "image_left", "image_right", "left", "right", "pairs", "is_match"]
+    ]
+
+    upload_dataset(args.dataset, df_pairs, id_fields=["locator_left", "locator_right"])
+
+
+def main() -> None:
+    ap = ArgumentParser()
+    ap.add_argument(
+        "--dataset",
+        type=str,
+        default=f"{DATASET} [{TASK}]",
+        help="Optionally specify a custom dataset name to upload.",
+    )
+    run(ap.parse_args())
+
+
+if __name__ == "__main__":
+    main()
