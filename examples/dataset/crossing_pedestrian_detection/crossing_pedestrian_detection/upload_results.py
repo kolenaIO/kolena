664bb0a6926065a425928c5c267fd90e1db8c013
Mac Bagwell <mac@kolena.com>
2024-06-24 16:01:37 -0700

    add comments

--- examples/dataset/crossing_pedestrian_detection/crossing_pedestrian_detection/upload_results.py
+++ examples/dataset/crossing_pedestrian_detection/crossing_pedestrian_detection/upload_results.py
@@ -103,10 +103,14 @@ def postprocess_inferences(inferences: List[ScoredPedestrianBoundingBox]) -> Lis
 
 def process_inf_data(action_model_name: str, detection_model_name: str) -> Dict[str, List[ScoredPedestrianBoundingBox]]:
     model_pkl_name = f"s3://{BUCKET}/{DATASET}/raw/jaad_{action_model_name}_{detection_model_name}_database.pkl"
+    # access S3 anonymously
+    # adapted from https://github.com/piskvorky/smart_open/blob/develop/howto.md#how-to-access-s3-anonymously
     with smart_open(model_pkl_name, "rb", transport_params=TRANSPORT_PARAMS) as inf_file:
         inf_annotations = pickle.load(inf_file)
 
     results_pkl = f"s3://{BUCKET}/{DATASET}/results/raw/{action_model_name}_{detection_model_name}.pkl"
+    # access S3 anonymously
+    # adapted from https://github.com/piskvorky/smart_open/blob/develop/howto.md#how-to-access-s3-anonymously
     with smart_open(results_pkl, "rb", transport_params=TRANSPORT_PARAMS) as results_file:
         results = pickle.load(results_file)
         predictions: Dict[str, Dict[int, Dict[str, float]]] = defaultdict(lambda: defaultdict(dict))
