ce5c6a8c7e2c78aca0608efee5f39a52976324b3
grant-Kolena <146848587+grant-Kolena@users.noreply.github.com>
2024-09-13 08:23:29 -0700

    Update search.py

--- kolena/_api/v2/search.py
+++ kolena/_api/v2/search.py
@@ -24,7 +24,7 @@ class Path(str, Enum):
 @dataclass(frozen=True)
 class UploadDatasetEmbeddingsRequest(BatchedLoad.WithLoadUUID):
     name: str
-    run_embedding_reduction: bool = True
+    run_embedding_reduction: bool
 
 
 @dataclass(frozen=True)
