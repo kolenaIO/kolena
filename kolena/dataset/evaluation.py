ed0bdb98deb7382763914307e11e9f070d7f118a
brianshen3 <brian.shen@kolena.com>
2024-10-31 11:50:45 -0400

    remove parenthese added failure detail

--- kolena/dataset/evaluation.py
+++ kolena/dataset/evaluation.py
@@ -343,8 +343,9 @@ def _upload_results(
         f"Total Rows Attempted: {total_rows}\n"
         f"Rows Successfully Inserted: {response.n_inserted}\n"
         f"Rows Successfully Updated: {response.n_updated}\n"
-        f"Rows Failed to Insert/Update: {total_rows - response.n_inserted - response.n_updated}\n"
-        f"Details: ({link})",
+        f"Rows Failed to Insert/Update: {total_rows - response.n_inserted - response.n_updated}"
+        f"Verify the id fields matches. Unchanged datapoints won't be modified. \n"
+        f"Details: {link}",
     )
     return response
 
