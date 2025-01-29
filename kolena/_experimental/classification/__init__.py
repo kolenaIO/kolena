c9a3ac39101bb9bf0a8c2650319db546d11e828c
Andrew Shi <munkyshi22@gmail.com>
2025-01-29 11:17:37 -0500

    Add client endpoint for augmenting multilabel classification results with datapoint metrics

--- kolena/_experimental/classification/__init__.py
+++ kolena/_experimental/classification/__init__.py
@@ -11,3 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from .dataset import upload_multilabel_classification_results
+
+__all__ = [
+    "upload_multilabel_classification_results",
+]
