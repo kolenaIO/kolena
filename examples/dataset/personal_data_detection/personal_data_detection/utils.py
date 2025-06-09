c76b4acc408d90f708c21340f57f2411d73cedbf
Nan Wei <nan@kolena.io>
2025-06-09 17:16:15 -0400

    util + test

---
+++ examples/dataset/personal_data_detection/personal_data_detection/utils.py
@@ -0,0 +1,73 @@
+# Copyright 2021-2025 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import torch
+from transformers import AutoModelForTokenClassification
+from transformers import AutoTokenizer
+
+PII_MODEL_NAME = "iiiorg/piiranha-v1-detect-personal-information"
+
+tokenizer = AutoTokenizer.from_pretrained(PII_MODEL_NAME)
+model = AutoModelForTokenClassification.from_pretrained(PII_MODEL_NAME)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+def detect_pii_in_dataframe() -> None:
+    pass
+
+
+def detect_pii_in_string(text: str) -> bool:
+    # Tokenize input text
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+
+    # Get the model predictions
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # Get the predicted labels
+    predictions = torch.argmax(outputs.logits, dim=-1)
+
+    # Convert token predictions to word predictions
+    encoded_inputs = tokenizer.encode_plus(text, return_offsets_mapping=True, add_special_tokens=True)
+    offset_mapping = encoded_inputs["offset_mapping"]
+
+    is_pii = False
+    pii_data_start = 0
+    pii_data_end = 0
+    pii_type = ""
+
+    for i, (start, end) in enumerate(offset_mapping):
+        if start == end:  # Special token
+            continue
+
+        label = predictions[0][i].item()
+        if label != model.config.label2id["O"]:  # Non-O label
+            current_pii_type = model.config.id2label[label]
+            if not is_pii:
+                is_pii = True
+                pii_data_start = start
+                pii_type = current_pii_type
+            elif pii_type == current_pii_type:
+                pii_data_end = end + 1
+            elif pii_type != current_pii_type:
+                # break out of the loop if the current pii data has finished scanning
+                break
+        elif is_pii:
+            break
+
+    if is_pii:
+        print(f"[{pii_type}] data detected: {text[pii_data_start:pii_data_end]}")
+        return True
+    return False
---
+++ examples/dataset/personal_data_detection/tests/test_utils.py
@@ -0,0 +1,38 @@
+# Copyright 2021-2025 Kolena Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from io import StringIO
+from typing import Optional
+from unittest.mock import patch
+
+import pytest
+from personal_data_detection.utils import detect_pii_in_string
+
+
+@pytest.mark.parametrize(
+    "text, expected_is_pii, expected_printed_message",
+    [
+        ("random", False, None),
+        ("My phone number is 5455-123-4567.", True, "[I-TELEPHONENUM] data detected:  5455-123-4567."),
+        ("My name is Obee Nobi.", True, "[I-GIVENNAME] data detected:  Obee"),
+        ("I live at 432423 Deka St, Tanooti. My phone number is ...", True, "[I-BUILDINGNUM] data detected:  432423"),
+    ],
+)
+def test__detect_pii_in_string(text: str, expected_is_pii: bool, expected_printed_message: Optional[str]) -> None:
+    with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+        is_pii = detect_pii_in_string(text)
+        printed_message = mock_stdout.getvalue()
+
+    assert is_pii == expected_is_pii
+    if expected_printed_message:
+        assert expected_printed_message in printed_message
